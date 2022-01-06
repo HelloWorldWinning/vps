f49352277fb25349a4ab65efd7fc793f4b97cc1d
HelloWorldWinning <feedzhulei@aol.com>
2022-01-07 02:50:52 +0800

    Update open_ipv6.sh

--- open_ipv6.sh
+++ open_ipv6.sh
@@ -26,3 +26,23 @@ iface eth0 inet6 dhcp
 EOF
 
 /etc/init.d/networking restart
+
+
+
+cat  >> /etc/sysctl.conf <<EOF 
+
+# forward ipv4
+net.ipv4.ip_forward = 1
+
+# forward ipv6
+net.ipv6.conf.all.accept_ra=2
+net.ipv6.conf.default.accept_ra=2
+net.ipv6.conf.eth0.accept_ra=2
+net.ipv6.conf.all.accept_ra=2
+net.ipv6.conf.eth0.forwarding=1
+net.ipv6.conf.all.forwarding=1
+
+EOF
+
+
+sysctl -p /etc/sysctl.conf
