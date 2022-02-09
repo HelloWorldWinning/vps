0775e6c7314ef732adfe4ae93b4c66192c915ba1
HelloWorldWinning <feedzhulei@aol.com>
2022-02-09 21:22:58 +0800

    Update isp_ip.sh

--- isp_ip.sh
+++ isp_ip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 IPV4=$(curl -4 ip.sb)
-read -p 'input ip; if Nothing input , this vps ip will be checked  : ' IPV42
+read -p 'ipdata.co; if Nothing input , this vps ip will be checked  : ' IPV42
 
 if  [ -z "$IPV42" ] ; then
       curl "https://api.ipdata.co/$IPV4?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8"|jq
