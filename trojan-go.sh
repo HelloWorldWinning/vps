1d6f51f54e97e3f79d1dae0c40f15626d9058a26
HelloWorldWinning <feedzhulei@aol.com>
2022-02-14 16:48:09 +0800

    Update trojan-go.sh

--- trojan-go.sh
+++ trojan-go.sh
@@ -202,7 +202,9 @@ getData() {
             CERT_FILE="/etc/trojan-go/${DOMAIN}.pem"
             KEY_FILE="/etc/trojan-go/${DOMAIN}.key"
         else
-            resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
+#            resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
+	    resolve=`curl -sL curl -sL ipget.net/?ip=${DOMAIN}}`
+	    
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 echo " ${DOMAIN} 解析结果：${resolve}"
