41cf15b0ac440c230c1da29f33211f415066a580
HelloWorldWinning <feedzhulei@aol.com>
2022-03-13 00:42:28 +0800

    Update open_ipv6.sh

--- open_ipv6.sh
+++ open_ipv6.sh
@@ -29,6 +29,16 @@ EOF
 
 
 
+cat  >> /etc/resolv.conf<<EOF 
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844
+
+EOF
+
+
+
 cat  >> /etc/sysctl.conf <<EOF 
 
 # forward ipv4
