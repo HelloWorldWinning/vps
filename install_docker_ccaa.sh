ffc8af078ab9e11d6f5320dbd87d04daf6eda294
HelloWorldWinning <feedzhulei@aol.com>
2022-03-28 11:41:47 +0800

    Update install_docker_ccaa.sh

--- install_docker_ccaa.sh
+++ install_docker_ccaa.sh
@@ -1,5 +1,5 @@
 docker run --name="ccaa" -d -p 6080:6080 -p 6081:6081 -p 6800:6800 -p 51413:51413 \
-    -v /down:/down \
+    -v /data/ccaaDown:/data/ccaaDown\
     -e PASS="1" \
     helloz/ccaa \
     sh -c "dccaa pass && dccaa start"
