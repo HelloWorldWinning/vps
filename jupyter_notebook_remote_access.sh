260dda355c9b8d03b7ee5a1f3c331651ad3aded2
HelloWorldWinning <feedzhulei@aol.com>
2022-04-01 17:26:00 +0800

    Create jupyter_notebook_remote_access.sh

---
+++ jupyter_notebook_remote_access.sh
@@ -0,0 +1,16 @@
+jupyter notebook --generate-config
+  
+cat >> /root/.jupyter/jupyter_notebook_config.py <<EOF 
+
+c.NotebookApp.allow_remote_access = True        #允许远程连接
+c.NotebookApp.open_browser = False              # 禁止自动打开浏览器
+c.NotebookApp.port =8888                        #任意指定一个端口
+c.NotebookApp.ip = '0.0.0.0' # listen on all IPs
+c.NotebookApp.allow_root= True
+#c.NotebookApp.ip='*'                            # 设置所有ip皆可访问
+#c.NotebookApp.password = u'sha1:.......'        #之前复制的密码
+
+EOF
+
+
+jupyter notebook password 
