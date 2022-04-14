9495ddf86c65971d76ac6ac245bc963a3e303c78
HelloWorldWinning <feedzhulei@aol.com>
2022-04-15 02:02:49 +0800

    Create latest_arm_kernel.sh

---
+++ latest_arm_kernel.sh
@@ -0,0 +1,18 @@
+sudo apt update && sudo apt upgrade -y
+Last_Version=$(apt-cache search linux-image  |grep arm64|grep v8|grep -v rt | tail -1 |awk '{print $1; exit}')
+
+echo $Last_Version
+apt -t bullseye-backports install $Last_Version
+
+
+# linux-image-5.16.0-0.bpo.4-arm64
+
+
+version=$(echo  $Last_Version | cut -d "-" -f 3)
+echo $version
+
+
+header=$(apt-cache search linux-header  |grep $version |grep -v rt|grep arm64|grep -v cloud|tail -1|awk '{print $1; exit}')
+
+echo $header
+apt -t bullseye-backports install $header
