af39ca3c3e995c4a0ea923cce8f2f57349c9617f
HelloWorldWinning <feedzhulei@aol.com>
2022-04-16 02:48:28 +0800

    Update goodv3.sh

--- goodv3.sh
+++ goodv3.sh
@@ -152,7 +152,7 @@ ${Red_font_prefix}41${Font_color_suffix} install docker
 ${Red_font_prefix}42${Font_color_suffix} install_docker_ccaa
 ${Red_font_prefix}43${Font_color_suffix} wget bashrc 。手工输入  source  ~/.bashrc
 ${Red_font_prefix}44${Font_color_suffix} enable jupyter_notebook_remote_access  jupyter notebook   --port=16666 --ip 0.0.0.0 --no-browser --allow-root
-${Red_font_prefix}45${Font_color_suffix}  升级到最新的 armv8 debian系统
+${Red_font_prefix}45${Font_color_suffix} 升级到最新的 armv8 debian系统
 
 
 ${Red_font_prefix}00${Font_color_suffix} exit
