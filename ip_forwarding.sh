2b09d92c6a99516ea204bdbb0b6210183315dc39
HelloWorldWinning <feedzhulei@aol.com>
2022-04-15 14:36:04 +0800

    Update ip_forwarding.sh

--- ip_forwarding.sh
+++ ip_forwarding.sh
@@ -6,11 +6,11 @@ net.ipv4.ip_forward = 1
 # forward ipv6
 net.ipv6.conf.all.accept_ra=2
 net.ipv6.conf.default.accept_ra=2
-net.ipv6.conf.eth0.accept_ra=2
+# net.ipv6.conf.eth0.accept_ra=2
 net.ipv6.conf.${net_card}.accept_ra=2
 
 net.ipv6.conf.all.accept_ra=2
-net.ipv6.conf.eth0.forwarding=1
+# net.ipv6.conf.eth0.forwarding=1
 net.ipv6.conf.${net_card}.forwarding=1
 
 net.ipv6.conf.all.forwarding=1
