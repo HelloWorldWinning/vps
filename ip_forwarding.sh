0f2809438329c67e87ec9f0a04d5661e1e6ead52
HelloWorldWinning <feedzhulei@aol.com>
2022-04-15 14:33:45 +0800

    Update ip_forwarding.sh

--- ip_forwarding.sh
+++ ip_forwarding.sh
@@ -1,3 +1,5 @@
+net_card=$(ip addr |grep BROADCAST|head -1|awk '{print $2; exit}'|cut -d ":" -f 1)
+
 cat  >> /etc/sysctl.conf <<EOF 
 # forward ipv4
 net.ipv4.ip_forward = 1
@@ -5,8 +7,12 @@ net.ipv4.ip_forward = 1
 net.ipv6.conf.all.accept_ra=2
 net.ipv6.conf.default.accept_ra=2
 net.ipv6.conf.eth0.accept_ra=2
+net.ipv6.conf.${net_card}.accept_ra=2
+
 net.ipv6.conf.all.accept_ra=2
 net.ipv6.conf.eth0.forwarding=1
+net.ipv6.conf.${net_card}.forwarding=1
+
 net.ipv6.conf.all.forwarding=1
 EOF
 
