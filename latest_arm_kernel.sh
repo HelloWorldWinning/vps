8c6457c8c65a9ea355b9ca37aa4bb23702c10e10
HelloWorldWinning <feedzhulei@aol.com>
2022-04-16 10:59:48 +0800

    Update latest_arm_kernel.sh

--- latest_arm_kernel.sh
+++ latest_arm_kernel.sh
@@ -4,14 +4,14 @@ EOF
 apt update
 apt upgrade -y
 
-apt -t bullseye-backports install linux-image-amd64 linux-headers-amd64 -y
+# apt -t bullseye-backports install linux-image-amd64 linux-headers-amd64 -y
 
-# Last_Version=$(apt-cache search linux-image  |grep arm64|grep v8|grep -v rt  |grep -v meta|grep -v unsigned|sort| tail -1 |awk '{print $1; exit}')
-# echo $Last_Version
-# apt -t bullseye-backports install $Last_Version
-# linux-image-5.16.0-0.bpo.4-arm64
-# version=$(echo  $Last_Version | cut -d "-" -f 3)
-# echo $version
-# header=$(apt-cache search linux-header  |grep $version |grep -v rt|grep arm64|grep -v cloud|tail -1|awk '{print $1; exit}')
-# echo $header
-# apt -t bullseye-backports install $header
+Last_Version=$(apt-cache search linux-image  |grep arm64|grep v8|grep -v rt  |grep -v meta|grep -v unsigned|sort| tail -1 |awk '{print $1; exit}')
+echo $Last_Version
+apt -t bullseye-backports install $Last_Version
+linux-image-5.16.0-0.bpo.4-arm64
+version=$(echo  $Last_Version | cut -d "-" -f 3)
+echo $version
+header=$(apt-cache search linux-header  |grep $version |grep -v rt|grep arm64|grep -v cloud|tail -1|awk '{print $1; exit}')
+echo $header
+apt -t bullseye-backports install $header
