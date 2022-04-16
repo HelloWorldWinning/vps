3eed0075efc3cec577eaf0b7c97d7d2aef5539ed
HelloWorldWinning <feedzhulei@aol.com>
2022-04-16 12:01:50 +0800

    Update latest_arm_kernel.sh

--- latest_arm_kernel.sh
+++ latest_arm_kernel.sh
@@ -6,7 +6,7 @@ apt upgrade -y
 
 # apt -t bullseye-backports install linux-image-amd64 linux-headers-amd64 -y
 
-Last_Version=$(apt-cache search linux-image  |grep arm64|grep v8|grep -v rt  |grep -v meta|grep -v unsigned|sort| tail -1 |awk '{print $1; exit}')
+Last_Version=$(apt-cache search linux-image  |grep arm64|grep v8|grep -v rt  |grep -v meta|grep -v unsigned|grep -v trunk|sort| tail -1 |awk '{print $1; exit}')
 echo $Last_Version
 apt -t bullseye-backports install $Last_Version
 linux-image-5.16.0-0.bpo.4-arm64
