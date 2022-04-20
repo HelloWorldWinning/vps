67c1ac322d946b8737474dfa8d49cd02423e061e
HelloWorldWinning <feedzhulei@aol.com>
2022-04-21 00:19:51 +0800

    Update update_trojan.sh

--- update_trojan.sh
+++ update_trojan.sh
@@ -2,8 +2,8 @@ crontab -l > conf && echo  -e "30 5 */7 * *   eval 'echo 3 \| bash <(curl -sL ht
  #crontab -l > conf && echo  -e "*/1 * * * *   eval 'echo 3 \| bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/trojan-go.sh)' >/root/tmp_trojan.txt" >> conf && crontab conf && rm -f conf
 
 cat  >> /etc/resolv.conf<<EOF 
-nameserver 8.8.8.8
 nameserver 8.8.4.4
-nameserver 2001:4860:4860::8888
+nameserver 8.8.8.8
 nameserver 2001:4860:4860::8844
+nameserver 2001:4860:4860::8888
 EOF
