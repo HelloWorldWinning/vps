a45771fa2d680637099c757a287ccc58226b24ad
HelloWorldWinning <feedzhulei@aol.com>
2022-04-21 16:04:01 +0800

    Update goodv3.sh

--- goodv3.sh
+++ goodv3.sh
@@ -10,6 +10,9 @@ net_card=$(ip addr |grep BROADCAST|head -1|awk '{print $2; exit}'|cut -d ":" -f 
 # 26)eval 'apt update;apt install -y wget curl vim tree lsof sudo htop rsync screen jq net-tools telnet' ;;
 
 
+disable_all_wg_servers='bash <(curl -sL    https://raw.githubusercontent.com/HelloWorldWinning/vps/main/disable_all_wg_servers.sh)'
+
+
 latest_arm_kernel='bash <(curl -sL    https://raw.githubusercontent.com/HelloWorldWinning/vps/main/latest_arm_kernel.sh)'
 
 
@@ -155,6 +158,7 @@ ${Red_font_prefix}42${Font_color_suffix} install_docker_ccaa
 ${Red_font_prefix}43${Font_color_suffix} wget bashrc 。手工输入  source  ~/.bashrc
 ${Red_font_prefix}44${Font_color_suffix} enable jupyter_notebook_remote_access  jupyter notebook   --port=16666 --ip 0.0.0.0 --no-browser --allow-root
 ${Red_font_prefix}45${Font_color_suffix} 升级到最新的 armv8 debian系统
+${Red_font_prefix}46${Font_color_suffix} systemctl stop and disable all wg
 
 
 ${Red_font_prefix}00${Font_color_suffix} exit
@@ -214,7 +218,7 @@ passwd root
 		43)eval "$bashrc";;
 		44)eval "$jupyter_notebook_remote_access";;
 		45)eval "$latest_arm_kernel";;
-		
+		46)eval "$disable_all_wg_servers";;
 
 		00)eval "exit";;
 		
