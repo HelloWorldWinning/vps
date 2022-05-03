9efad76907ef296e9a5ca354bf56c7d7b22e5cd5
HelloWorldWinning <feedzhulei@aol.com>
2022-05-03 15:59:22 +0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -9,7 +9,11 @@ sudo adduser ${rdp_username}
 
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g"  /etc/ssh/sshd_config
 sudo systemctl restart sshd
-sudo apt-get install net-tools xrdp xfce4 tigervnc-standalone-server
+
+sudo apt update -y
+#sudo apt-get install net-tools xrdp xfce4 tigervnc-standalone-server -y
+# sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+sudo apt install net-tools xrdp xfce4 tigervnc-standalone-server  xfce4-goodies xorg dbus-x11 x11-xserver-utils  -y
 
 echo xfce4-session>/home/${rdp_username}/.xsession
 
