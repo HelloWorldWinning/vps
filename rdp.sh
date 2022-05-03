972ae5f5888306d8f041ca4e0fc4958851e96035
HelloWorldWinning <feedzhulei@aol.com>
2022-05-03 23:48:16 +0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,23 +1,37 @@
-# https://zhuanlan.zhihu.com/p/76991840  安装Debian并开启远程桌面（通过Xorg）
-# https://linuxize.com/post/how-to-install-xrdp-on-debian-10/  How to Install Xrdp Server (Remote Desktop) on Debian 10
-read -p 'input rdp user name[rdp for empty]': rdp_username 
-if [[ -z "${rdp_username}" ]] ; then
+apt update -y
+apt install sudo curl wget  -y
+sudo apt-get update -y
+sudo apt-get install net-tools xrdp xfce4 tigervnc-standalone-server -y
+
+
+
+
+sudo service xrdp stop
+
+read -p 'input rdp port(default 33389)': rdp_port_input
+if [[ -z "${rdp_port_input}" ]] ; then
+ rdp_port=33389
+else
+rdp_port=$rdp_port_input
+fi
+
+sudo sed -i "s/port=3389/port=${rdp_port}/g" /etc/xrdp/xrdp.ini
+
+sudo service xrdp restart
+
+
+
+read -p 'input rdp user name[rdp for empty]': rdp_username_input
+if [[ -z "${rdp_username_input}" ]] ; then
  rdp_username=rdp
 else
-rdp_username=$rdp_username
+ rdp_username=$rdp_username_input
 fi
 sudo adduser ${rdp_username}
 sudo adduser ${rdp_username} ssl-cert  
-
-sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g"  /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
-sudo apt update -y
-#sudo apt-get install net-tools xrdp xfce4 tigervnc-standalone-server -y
-# sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
-sudo apt install net-tools  xfce4 tigervnc-standalone-server  xfce4-goodies xorg dbus-x11 x11-xserver-utils   -y
-sudo apt install xrdp -y
-
+cat  >>/etc/sudoers<<EOF 
+${rdp_username} ALL=(ALL:ALL) ALL
+EOF
 
 echo xfce4-session>/home/${rdp_username}/.xsession
 
@@ -25,10 +39,43 @@ sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/
 
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
-sudo cat  >>/etc/sudoers<<EOF 
-${rdp_username}   ALL=(ALL:ALL) ALL
-EOF
 
 
 
-sudo systemctl status xrdp
+
+
+## https://zhuanlan.zhihu.com/p/76991840  安装Debian并开启远程桌面（通过Xorg）
+## https://linuxize.com/post/how-to-install-xrdp-on-debian-10/  How to Install Xrdp Server (Remote Desktop) on Debian 10
+#read -p 'input rdp user name[rdp for empty]': rdp_username 
+#if [[ -z "${rdp_username}" ]] ; then
+# rdp_username=rdp
+#else
+#rdp_username=$rdp_username
+#fi
+#sudo adduser ${rdp_username}
+#sudo adduser ${rdp_username} ssl-cert  
+#
+#sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g"  /etc/ssh/sshd_config
+#sudo systemctl restart sshd
+#
+#sudo apt update -y
+##sudo apt-get install net-tools xrdp xfce4 tigervnc-standalone-server -y
+## sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+#sudo apt install net-tools  xfce4 tigervnc-standalone-server  xfce4-goodies xorg dbus-x11 x11-xserver-utils   -y
+#sudo apt install xrdp -y
+#
+#
+#echo xfce4-session>/home/${rdp_username}/.xsession
+#
+#sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#
+#sudo apt install ./google-chrome-stable_current_amd64.deb -y
+#
+#sudo cat  >>/etc/sudoers<<EOF 
+#${rdp_username}   ALL=(ALL:ALL) ALL
+#EOF
+#
+#
+#
+#sudo systemctl status xrdp
+#
