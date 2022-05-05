f9fdf95b4eef9a1838b2b08724d76bf5ca863401
HelloWorldWinning <feedzhulei@aol.com>
2022-05-05 13:09:06 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,20 @@
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+echo '
+已经清空所有 规则，允许所有端口
+非常好的iptables详解 规则 
+https://www.jianshu.com/p/ee4ee15d3658
+
 iptables -P INPUT ACCEPT  #   (DROP|ACCEPT)  默认是关的/默认是开的
 
 /usr/sbin/iptables  -D INPUT  1
+iptables -I INPUT -p udp --dport  53848 -j DROP
+iptables -I INPUT -p udp --destination-port  53848 -j DROP
 
 iptables -I INPUT -j ACCEPT 
  
@@ -15,3 +29,4 @@ https://qa.1r1g.cn/superuser/ask/44412861/
 
 iptables -P INPUT ACCEPT iptables 说明
 http://blog.chinaunix.net/uid-31410005-id-5775932.html
+'
