74b1d6352bb86f38a6f7feb4ef0f708f81d59ee8
HelloWorldWinning <feedzhulei@aol.com>
2022-05-20 18:36:42 +0800

    Update update_trojan.sh

--- update_trojan.sh
+++ update_trojan.sh
@@ -7,3 +7,8 @@ nameserver 8.8.8.8
 nameserver 2001:4860:4860::8844
 nameserver 2001:4860:4860::8888
 EOF
+
+inet_ip=$(cat  /etc/hosts|grep debian |awk '{print $1}')
+cat  >> /etc/hosts<<EOF 
+${inet_ip} $HOSTNAME
+EOF
