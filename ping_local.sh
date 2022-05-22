c480a94031bbc9df38abe734212d4590c75f0ef4
HelloWorldWinning <feedzhulei@aol.com>
2022-05-23 00:16:33 +0800

    Update ping_local.sh

--- ping_local.sh
+++ ping_local.sh
@@ -1,34 +1,15 @@
 #!/usr/local/bin/bash
 n=5
 
-out=$(ping m3.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+
+
+out=$(ping gcp.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
 loss=$(echo $out | cut -d ' ' -f 12)
 ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
 echo $loss $ping $site  
 
-out=$(ping m3ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-site=$(echo $out | cut -d ' ' -f 2)
-loss=$(echo $out | cut -d ' ' -f 12)
-ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-echo $loss $ping $site  
-
-out=$(ping m1ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-site=$(echo $out | cut -d ' ' -f 2)
-loss=$(echo $out | cut -d ' ' -f 12)
-ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-echo $loss $ping $site  
-
-
-out=$(ping m1.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-site=$(echo $out | cut -d ' ' -f 2)
-loss=$(echo $out | cut -d ' ' -f 12)
-ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-echo $loss $ping $site  
-
-
-
-out=$(ping az3.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+out=$(ping az.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
 loss=$(echo $out | cut -d ' ' -f 12)
 ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
@@ -43,8 +24,66 @@ ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
 echo $loss $ping $site  
 
 
-out=$(ping gcp.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+
+out=$(ping cml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
 loss=$(echo $out | cut -d ' ' -f 12)
 ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
 echo $loss $ping $site  
+
+out=$(ping c.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+out=$(ping c2.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+out=$(ping m3.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+out=$(ping m3ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+out=$(ping m2.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+out=$(ping m2ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+out=$(ping m1ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+
+out=$(ping m1ml.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+out=$(ping m1.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site 
