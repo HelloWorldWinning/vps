a8210446064264e4c0e0052ea247f56fd5e2937c
HelloWorldWinning <feedzhulei@aol.com>
2022-05-27 18:13:43 +0800

    Update goodv3.sh

--- goodv3.sh
+++ goodv3.sh
@@ -131,7 +131,7 @@ openvpn='bash <(curl -sL https://raw.githubusercontent.com/angristan/openvpn-ins
 v2ray='bash <(curl -s -L https://git.io/v2ray.sh)'
 kcptun='wget --no-check-certificate https://github.com/kuoruan/shell-scripts/raw/master/kcptun/kcptun.sh &&chmod +x ~/kcptun.sh &&bash ~/kcptun.sh'
 ss_go='wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/ss-go.sh && chmod +x ss-go.sh && bash ss-go.sh'
-ss_latest='bash <(curl -sL https://s.hijk.art/ss.sh)'
+ss_latest='bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ss.sh)'
 ssr='wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh'
  
 
