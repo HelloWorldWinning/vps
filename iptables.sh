fe5f41eff6105f3887ada4c8782fa62e73bcb143
HelloWorldWinning <feedzhulei@aol.com>
2022-06-07 15:58:24 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -17,35 +17,24 @@ netfilter-persistent save
 echo  "https://raw.githubusercontent.com/HelloWorldWinning/vps/main/iptables.sh"
 
 
-echo
-"
-
+echo '''
 已经清空所有 规则，允许所有端口
 非常好的iptables详解 规则 
 https://www.jianshu.com/p/ee4ee15d3658
-
 iptables -P INPUT ACCEPT  #   (DROP|ACCEPT)  默认是关的/默认是开的
-
 /usr/sbin/iptables  -D INPUT  1
 iptables -I INPUT -p udp --dport  53848 -j DROP
 iptables -I INPUT -p udp --destination-port  53848 -j DROP
-
 iptables -I INPUT -j ACCEPT 
  
 iptables -I  INPUT -p tcp --dport 8090 -j ACCEPT  #插入在链的第一位置
-
 iptables -A INPUT -p tcp --dport 20 -j ACCEPT  #追加到链的末尾
-
 iptables -I INPUT -p udp --destination-port  53848 -j DROP
-
 需要 iptables 规则来接受所有传入的流量
 https://qa.1r1g.cn/superuser/ask/44412861/
-
 iptables -P INPUT ACCEPT iptables 说明
 http://blog.chinaunix.net/uid-31410005-id-5775932.html
-
 LinuxONE VPS
 https://owo.misaka.rest/linux1-ss/ 
 
-
-"
+'''
