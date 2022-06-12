b1ca1c8269b47ce0cf8b35be814e6686c1c05f29
HelloWorldWinning <feedzhulei@aol.com>
2022-06-12 23:24:09 +0800

    Update ping_local.sh

--- ping_local.sh
+++ ping_local.sh
@@ -7,6 +7,29 @@ n=${PING_N_input}
 fi
 
 
+
+
+
+
+out=$(ping g1.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+
+
+out=$(ping g2.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
+site=$(echo $out | cut -d ' ' -f 2)
+loss=$(echo $out | cut -d ' ' -f 12)
+ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
+echo $loss $ping $site  
+
+
+
+
+
 out=$(ping gtw.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
 loss=$(echo $out | cut -d ' ' -f 12)
