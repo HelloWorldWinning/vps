a89ea2019ddf35a6742ec090ecfb3fc18f73f011
HelloWorldWinning <feedzhulei@aol.com>
2022-06-14 10:27:46 +0800

    Update ping_local.sh

--- ping_local.sh
+++ ping_local.sh
@@ -17,30 +17,9 @@ loss=$(echo $out | cut -d ' ' -f 12)
 ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
 echo $loss $ping $site  
 
+ 
 
-
-
-#out=$(ping g2.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-#site=$(echo $out | cut -d ' ' -f 2)
-#loss=$(echo $out | cut -d ' ' -f 12)
-#ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-#echo $loss $ping $site  
-
-
-
-
-
-out=$(ping gtw.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-site=$(echo $out | cut -d ' ' -f 2)
-loss=$(echo $out | cut -d ' ' -f 12)
-ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-echo $loss $ping $site  
-
-out=$(ping gcp.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
-site=$(echo $out | cut -d ' ' -f 2)
-loss=$(echo $out | cut -d ' ' -f 12)
-ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
-echo $loss $ping $site  
+ 
 
 out=$(ping az.wardao.xyz -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
