c046ad9655d8357519d9bf0af16be87ce21ae67a
HelloWorldWinning <feedzhulei@aol.com>
2022-06-14 12:35:41 +0800

    Create jobs_spiders.sh

---
+++ jobs_spiders.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/bash
+
+aptitude install  -y chromium-driver  msmtp mutt
+
+
+cat >>~/.bashrc <<EOF
+export PATH="/root/anaconda3/bin:$PATH"
+EOF
+source ~/.bashrc 
+
+wget -O /root/Anaconda3-2022.05-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+bash  /root/Anaconda3-2022.05-Linux-x86_64.sh
+
+pip install  html5lib selenium
+
+
+
+ 
+
+cat >>/root/.msmtprc <<EOF
+tls on
+tls_starttls on
+tls_certcheck off
+protocol smtp
+auth on
+
+#account gmail
+host smtp.gmail.com
+domain gmail.com
+port 587
+logfile   ~/.msmtp.log
+user work100100@gmail.com
+from work100100@gmail.com
+password vmxjjnvkhkhfdqsp
+#password qiubojunKJL12345
+#password mmamhaodrvokcvhc
+#tls_trust_file /etc/ssl/certs/ca-certificates.crt
+# account gmail
+
+EOF
+ 
+
+
+cat >>/root/.muttrc <<EOF
+set editor="vim"
+set sendmail="/usr/bin/msmtp"
+set use_from=yes
+set realname="work100100@gmail"
+set from=work100100@gmail.com
+set envelope_from=yes
+set crypt_use_gpgme=no
+
+EOF
+
+
+
+
+
+################################################
+#aptitude install  -y chromium-driver  msmtp mutt
+#cat >>~/.bashrc <<EOF
+#export PATH="/root/anaconda3/bin:$PATH"
+#EOF
+#source ~/.bashrc 
+#pip install  html5lib selenium
