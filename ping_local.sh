9a3c3f9f6a4f8eac71f0eec19c849c1a76ea41bc
HelloWorldWinning <feedzhulei@aol.com>
2022-07-31 23:13:51 +0800

    Update ping_local.sh

--- ping_local.sh
+++ ping_local.sh
@@ -1,21 +1,38 @@
 #!/usr/local/bin/bash
 
 SITES=(
+"comment:GCP"
 "uloveme.eu.org"
 "g.wardao.xyz"
 
+"comment:AZ"
 "az.wardao.xyz"
 "ajp.wardao.xyz"
 
+"comment:AWS"
 "jp.wardao.xyz"
 "sg.wardao.xyz"
 "sgec2.wardao.xyz"
 
- 
+"comment:others"
 "ibm1.wardao.xyz"
 
 )
 
+
+
+
+
+Green_font_prefix="\033[32m"
+Red_font_prefix="\033[31m"
+Font_color_suffix="\033[0m"
+
+# Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+ 
+
+#string_all="123222 32 232"
+#string_unwanted="32"
+#string_wanted=${string_all//$string_unwanted/}
 
 
 
@@ -29,12 +46,19 @@ fi
 
  
 for a_site in ${SITES[*]}; do
-       
+if [[ $a_site == *"comment"* ]]; then
+  #echo "It's there!"
+#  echo "comment:gcp" | cut -d ":" -f 2
+  name_vps=$(echo $a_site | cut -d ":" -f 2)
+  echo -e "========================   ${Red_font_prefix}${name_vps}${Font_color_suffix}   ========================"
+
+else     
 out=$(ping $a_site -c ${n} | grep -E 'loss$|^---|^round')
 site=$(echo $out | cut -d ' ' -f 2)
 loss=$(echo $out | cut -d ' ' -f 12)
 ping=$(echo $out | cut -d ' ' -f 18  | sed "s/\.[0-9][0-9][0-9]//g" )
 echo $loss $ping $site  
+fi
 
 done
 
