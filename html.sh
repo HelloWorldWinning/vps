cfae6138cc10ae5c0dd9231cfc13876ff61e7ab0
HelloWorldWinning <feedzhulei@aol.com>
2022-08-12 02:02:40 +0800

    Update html.sh

--- html.sh
+++ html.sh
@@ -1,5 +1,12 @@
 echo "tree -H '.' -L 1 --noreport --charset utf-8 -o index.html"
 
+echo "
+    location /f/ {
+    alias /root/shared/;     
+    index index.html;
+    }
+"
+
 #
 #read -p 'level of tree default=1 =>': level ;
 #[[ -z "${level}" ]] && level=1
