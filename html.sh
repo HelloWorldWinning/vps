14be015a3e9fa3055c008f68c09633509e2969c0
HelloWorldWinning <feedzhulei@aol.com>
2022-08-11 21:25:04 +0800

    Update html.sh

--- html.sh
+++ html.sh
@@ -1,1 +1,9 @@
-tree -H '.' -L 1 --noreport --charset utf-8 > index.html
+read -p 'level of tree default=1 =>': level ;
+[[ -z "${level}" ]] && level=1
+
+
+
+read -p 'path default current =>': path ;
+[[ -z "${path}" ]] && path=$(pwd)
+
+tree -H ${path} -L ${level} --noreport --charset utf-8 > ${path}/index.html
