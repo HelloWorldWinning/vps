c4e23e5456dd808c99a3936ee88f238449134f13
HelloWorldWinning <feedzhulei@aol.com>
2022-08-12 09:05:21 +0800

    Update html.sh

--- html.sh
+++ html.sh
@@ -1,5 +1,3 @@
-echo "tree -H '.' -L 1 --noreport --charset utf-8 -o index.html"
-
 echo "
     location /f/ {
     alias /root/shared/;     
@@ -7,11 +5,19 @@ echo "
     }
 "
 
+echo "
+tree -H '.' -L 1 --noreport --charset utf-8 -o index.html
+"
 
 read -p 'level of tree default=1 =>' level ;
 [[ -z "${level}" ]] && level=1
 
-read -p 'path default current =>' path ;
-[[ -z "${path}" ]] && path=$(pwd)
+tree -H '.' -L  ${level}  --noreport --charset utf-8 -o index.html
 
-tree -H '.'  -L ${level} --noreport --charset utf-8 > ${path}/index.html
+
+
+
+
+#read -p 'path default current =>' path ;
+#[[ -z "${path}" ]] && path=$(pwd)
+#tree -H '.'  -L ${level} --noreport --charset utf-8 > ${path}/index.html
