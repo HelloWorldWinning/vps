c24d07721981a7dcd568dc97b37056b48cdbf75e
HelloWorldWinning <feedzhulei@aol.com>
2022-08-18 14:13:38 +0800

    Update update_trojan.sh

--- update_trojan.sh
+++ update_trojan.sh
@@ -1,5 +1,5 @@
-crontab -l > conf && echo  -e "30 5 */7 * *   eval 'echo 3 | bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/trojan-go.sh)' >/root/tmp_trojan.txt" >> conf && crontab conf && rm -f conf
- #crontab -l > conf && echo  -e "*/1 * * * *   eval 'echo 3 \| bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/trojan-go.sh)' >/root/tmp_trojan.txt" >> conf && crontab conf && rm -f conf
+crontab -l > conf && echo  -e "30 5 */7 * *   eval 'echo "3" | bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/trojan-go.sh)' >/root/tmp_trojan_update.txt" >> conf && crontab conf && rm -f conf
+crontab -l > conf && echo  -e "30 5 * * *   eval 'echo "14" | bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/xray_mianliu.sh)' >/root/tmp_xray_restart.txt" >> conf && crontab conf && rm -f conf
 
 cat  >> /etc/resolv.conf<<EOF 
 nameserver 8.8.4.4
@@ -12,3 +12,6 @@ inet_ip=$(cat  /etc/hosts|grep debian |awk '{print $1}')
 cat  >> /etc/hosts<<EOF 
 ${inet_ip} $HOSTNAME
 EOF
+
+
+ #crontab -l > conf && echo  -e "*/1 * * * *   eval 'echo 3 \| bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/trojan-go.sh)' >/root/tmp_trojan.txt" >> conf && crontab conf && rm -f conf
