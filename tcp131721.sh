dab67c28257e0d5335ce6ca293c13077ea25ed8b
root <feedzhulei@aol.com>
2022-09-11 16:21:03 +0800

    sudo

--- tcp131721.sh
+++ tcp131721.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+apt install -y sudo
 #=================================================
 #	System Required: CentOS 7/8,Debian/ubuntu,oraclelinux
 #	Description: BBR+BBRplus+Lotserver
