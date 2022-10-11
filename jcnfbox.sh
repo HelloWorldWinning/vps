9cbc397227313b9158511ee9992d81524531e508
root <feedzhulei@aol.com>
2022-10-11 02:52:04 -0400

    not reboot

--- jcnfbox.sh
+++ jcnfbox.sh
@@ -185,8 +185,8 @@ sysctl -p
 	read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
 	[ -z "${yn}" ] && yn="y"
 	if [[ $yn == [Yy] ]]; then
-		echo -e "${Info} VPS 重启中..."
-		reboot
+		echo -e "${Info} VPS not! 重启中..."
+		#reboot
 	fi
 }
 
--- tcpx_modified.sh
+++ tcpx_modified.sh
@@ -802,8 +802,8 @@ net.ipv4.tcp_max_orphans = 32768
   read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
   [ -z "${yn}" ] && yn="y"
   if [[ $yn == [Yy] ]]; then
-    echo -e "${Info} VPS 重启中..."
-    reboot
+    echo -e "${Info} VPS not!! 重启中..."
+    #reboot
   fi
 }
 
