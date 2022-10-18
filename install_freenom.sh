0af9b0de1b6948a4c258f6a2d106a264bfc26d9d
HelloWorldWinning <feedzhulei@aol.com>
2022-10-18 09:54:15 +0800

    new Developer api

--- install_freenom.sh
+++ install_freenom.sh
@@ -2,7 +2,8 @@ docker run -d --name freenom --restart always -v $(pwd):/conf -v $(pwd)/logs:/ap
 
 #wget  -O /root/.env  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/env_freenom.txt
 
-wget -O /root/.env  --header "Authorization: token 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" https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAAB2CDARNMWBH4IIHVIPF35FMY2N7THQ
+#wget -O /root/.env  --header "Authorization: token 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" https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAAB2CDARNMWBH4IIHVIPF35FMY2N7THQ
+wget -O /root/.env  --header "Authorization: token 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" https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAAB2CDARNMWBH4IIHVIPF35FMY2N7THQ
 
 docker restart freenom
 sleep 9
