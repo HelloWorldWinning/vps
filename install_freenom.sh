1f89d80988c0ec605b1f718a9619611fab6632fb
root <feedzhulei@aol.com>
2022-10-17 23:22:43 -0400

    read -p passowrd

--- install_freenom.sh
+++ install_freenom.sh
@@ -5,7 +5,13 @@ docker run -d --name freenom --restart always -v $(pwd):/conf -v $(pwd)/logs:/ap
 
 #wget -O /root/.env  --header "Authorization: token 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" https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAAB2CDARNMWBH4IIHVIPF35FMY2N7THQ
 
-wget  -O /root/.env  --header "Authorization:token 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"  -r  "https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAABYTXSAAZFGPNS7QLYUC5JTYY2ODB7A"
+#wget  -O /root/.env  --header "Authorization:token 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"  -r  "https://raw.githubusercontent.com/HelloWorldWinning/vps_private/main/env_freenom.txt?token=GHSAT0AAAAAABYTXSAAZFGPNS7QLYUC5JTYY2ODB7A"
+
+wget  -O /root/.env   https://raw.githubusercontent.com/HelloWorldWinning/vps/main/env_freenom.txt
+
+read -p 'password for freenom.com':FNpassword;
+
+sed -i 's/RepLAce/${FNpassword}/g' /root/.env
 
 docker restart freenom
 sleep 9
