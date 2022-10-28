a6558572f054bd2deedab14304033bf239102e53
root <feedzhulei@aol.com>
2022-10-28 11:38:24 -0400

    hostname added

--- goodv3.sh
+++ goodv3.sh
@@ -378,7 +378,11 @@ others for input location
 		69)eval "$aws_arm_dd";;
                 70)eval "$docker_socks5_serjs" ;;
                 71)eval "$install_freenom" ;;
-                72)read -p 'user name =>': USER_NAME &&  hostnamectl set-hostname $USER_NAME ;;
+                72)read -p 'user name =>': USER_NAME &&  hostnamectl set-hostname $USER_NAME
+cat >>/etc/hosts<<EOF
+$(ip route get 1.2.3.4 | awk '{print $7}')   $('hostname'  )
+EOF       
+			;;
 
 		73)nc -l 59797  | tar xfvz - ;;
 		74)read -p 'ip or domain =>': IPIP && tar cfzv  - *  | nc -q 1 ${IPIP} 59797 ;;
