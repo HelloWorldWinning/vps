a8aa137c5f5e41693cc696448611ea07be45e68a
root <feedzhulei@aol.com>
2022-10-28 16:54:02 +0800

    getData

--- xray_mianliu.sh
+++ xray_mianliu.sh
@@ -559,11 +559,11 @@ getData() {
         colorEcho ${YELLOW} "  2. 伪装域名DNS解析指向当前服务器ip（${IP}）"
         colorEcho ${BLUE} "  3. 如果/root目录下有 xray.pem 和 xray.key 证书密钥文件，无需理会条件2"
         echo " "
-        read -p " 确认满足按y，按其他退出脚本：" answer
-        if [[ "${answer,,}" != "y" ]]; then
-            exit 0
-        fi
-
+#        read -p " 确认满足按y，按其他退出脚本：" answer
+#        if [[ "${answer,,}" != "y" ]]; then
+#            exit 0
+#        fi
+#
         echo ""
         while true
         do
