0bc8841e32f60f790315f4342579ebf1595831fd
root <feedzhulei@aol.com>
2022-10-30 03:49:50 +0800

    new33333

--- xray_mianliu.sh
+++ xray_mianliu.sh
@@ -613,8 +613,8 @@ getData() {
         fi
         colorEcho ${BLUE}  " xray端口：$PORT"
     else
-        read -p " 请输入Nginx监听端口[100-65535的一个数字，默认443]：" PORT
-        [[ -z "${PORT}" ]] && PORT=443
+        read -p " 请输入Nginx监听端口[100-65535的一个数字，默认80]：" PORT
+        [[ -z "${PORT}" ]] && PORT=80
         if [ "${PORT:0:1}" = "0" ]; then
             colorEcho ${BLUE}  " 端口不能以0开头"
             exit 1
