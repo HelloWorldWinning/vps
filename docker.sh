8b67f06e5d2d75572eb1dac46abe113c0a117a0c
root <feedzhulei@aol.com>
2022-11-01 13:29:13 +0800

    new

--- docker.sh
+++ docker.sh
@@ -1,15 +1,34 @@
-sudo apt-get update  -y
+apt install sudo -y
+sudo apt-get -y purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+
+
+sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release -y
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update   -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io  -y
+
+sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+docker ps -a
+
 #apt-cache madison docker-ce
-sudo docker run hello-world 
+#sudo docker run hello-world 
