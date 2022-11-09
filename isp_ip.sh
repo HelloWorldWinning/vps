c48fb4e0322f245240839b2d569fa3694c592f07
root <feedzhulei@aol.com>
2022-11-09 15:12:43 +0800

    new

--- isp_ip.sh
+++ isp_ip.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
-IPV4=$(curl -4 ip.sb)
+apt install -y jq >/dev/null 2>&1
+
+IPV4=$(curl -sSLk -4 ip.sb) >/dev/null 2>&1
+
 read -p 'ipdata.co; if Nothing input , this vps ip will be checked: ' IPV42
 
-IPV42=$(ping  -4  -c 1 "${IPV42}"  |head -1 | awk '{print $3}'  | tr -d '(|)')
 
 if  [ -z "$IPV42" ] ; then
-      curl "https://api.ipdata.co/$IPV4?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8"|jq
+      curl -sSLk "https://api.ipdata.co/$IPV4?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8"|jq
 
      else
 
-     curl "https://api.ipdata.co/$IPV42?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8" |jq
+     IPV42=$(ping  -4  -c 1 "${IPV42}"  |head -1 | awk '{print $3}'  | tr -d '(|)') >/dev/null 2>&1
+     curl -sSLk  "https://api.ipdata.co/$IPV42?api-key=513d4b07583037a5a89b6cff4ebff0083bef180977dc71dd73804cf8" |jq
 
 fi
