ce8d1e37f2c7a6af9a20f3e9ef99e67f5dcf6c1d
root <feedzhulei@aol.com>
2022-11-16 02:28:41 +0800

    new

--- xray_mianliu.sh
+++ xray_mianliu.sh
@@ -570,7 +570,7 @@ getData() {
         echo ""
         while true
         do
-            read -p "请输入伪装域名：" DOMAIN
+            read -p "请输入伪装域名:" DOMAIN
             if [[ -z "${DOMAIN}" ]]; then
                 colorEcho ${RED} " 域名输入错误，请重新输入！"
             else
@@ -578,7 +578,7 @@ getData() {
             fi
         done
         DOMAIN=${DOMAIN,,}
-        colorEcho ${BLUE}  "伪装域名(host)：$DOMAIN"
+        colorEcho ${BLUE}  "伪装域名(host):$DOMAIN"
 
         echo ""
         if [[ -f ~/xray.pem && -f ~/xray.key ]]; then
