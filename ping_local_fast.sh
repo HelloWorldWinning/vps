a90e16806d99577228bda7bd89ba399a3f6e5923
root <feedzhulei@aol.com>
2022-11-23 04:26:46 -0500

    new

--- ping_local_fast.sh
+++ ping_local_fast.sh
@@ -25,7 +25,10 @@ site6=(
 "66sanjose.hardeasy.top"
 )
 
-
+IPV4_local=(dig @ns1.google.com TXT o-o.myaddr.l.google.com +short -4 |tr -d \")
+IPV6_local=IPV6_local=(dig @ns1.google.com TXT o-o.myaddr.l.google.com +short -6 |tr -d \")
+echo "local Public IPV4 : $IPV4_local"
+echo "local Public IPV6 : $IPV6_local"
 
 Green_font_prefix="\033[32m"
 Red_font_prefix="\033[31m"
