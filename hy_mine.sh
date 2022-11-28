234a0b84495c29a0bf288ab50505b87bd928f493
root <feedzhulei@aol.com>
2022-11-28 14:54:06 +0800

    fullchain.cer

--- hy_mine.sh
+++ hy_mine.sh
@@ -1,3 +1,78 @@
+Check_Domain_Resolve () {
+IPV4=$(dig  +time=1 +tries=2   @1.1.1.1 +short  txt ch  whoami.cloudflare  |tr -d \")
+IPV6=$(dig  +time=1 +tries=2  +short @2606:4700:4700::1111 -6 ch txt whoami.cloudflare|tr -d \")
+resolve4="$(dig  +time=1 +tries=2  A  +short ${Domain} @1.1.1.1)"
+resolve6="$(dig  +time=1 +tries=2  AAAA +short ${Domain} @1.1.1.1)"
+res4=`echo -n ${resolve4} | grep $IPV4`
+res6=`echo -n ${resolve6} | grep $IPV6`
+res=`echo $res4$res6`
+echo "======"
+echo "$res"
+IP=`echo $res4$res6`
+echo "${Domain}  points to: $res"
+            if [[ -z "${res}" ]]; then
+                echo " ${Domain} 解析结果：${res}"
+                echo -e " ${RED}伪装域名未解析到当前服务器IP $IPV4$IPV6 !${PLAIN}"
+                exit 1
+               else
+                    echo "$Domain successfully resolved to $res "
+            fi
+}
+
+
+
+Acme_Get(){
+
+apt install socat -y
+curl -sL https://get.acme.sh | sh -s email=hijk.pw@protonmail.ch
+source ~/.bashrc
+~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh   --issue -d $Domain --keylength ec-256 --force  --standalone --listen-v6
+
+}
+
+Get_Key_Path(){
+
+echo "如果~/.acme.sh下没有正确域名 ，请确保80端口没有被占用，脚本自动获取域名"
+  
+read -p "请正确输入域名: " Domain
+#echo "输入的域名为：$Domain"
+Check_Domain_Resolve 
+
+#cer_path=/root/.acme.sh/${Domain}_ecc/${Domain}.cer
+cer_path=/root/.acme.sh/${Domain}_ecc/fullchain.cer
+key_path=/root/.acme.sh/${Domain}_ecc/${Domain}.key
+
+if [[ -f $cer_path ]]  && [[ -f $key_path ]]  ; then
+echo $cer_path
+echo $key_path
+
+else
+
+Acme_Get
+
+      if [[ -f $cer_path ]]  && [[ -f $key_path ]]  ; then
+    	echo $cer_path
+    	echo $key_path
+      else
+
+	echo  "/root/.acme.sh/${Domain}_ecc/ 不存在cer key"	
+	exit 1
+
+      fi
+fi
+
+
+}
+
+
+
+
+
+
+
+
 function echoColor() {
 	case $1 in
 		# 红色
@@ -100,30 +175,33 @@ function  install_config_hy(){
 
 bash  <(curl -Ls https://raw.githubusercontent.com/HyNetwork/hysteria/master/install_server.sh)
 
-while true
-        do
-            read -p " 请输入cer path：" cert_path
-            if [ ! -f "${cert_path}" ]; then
-                echoColor red " certificate path wrong，请重新输入！"
-				echoColor green "请输入证书cert文件路径(需fullchain):"
-            else
-                break
-            fi
-        done
-
-while true
-        do
-            read -p " 请输入key path：" key_path
-            if [ !  -f "${key_path}" ]; then
-                echoColor red " key path wrong，请重新输入！"
-				echoColor green "请输入证书key文件路径(需fullchain):"
-            else
-                break
-            fi
-        done
+Get_Key_Path
 
 
-
+#while true
+#        do
+#            read -p " 请输入cer path：" cert_path
+#            if [ ! -f "${cert_path}" ]; then
+#                echoColor red " certificate path wrong，请重新输入！"
+#				echoColor green "请输入证书cert文件路径(需fullchain):"
+#            else
+#                break
+#            fi
+#        done
+#
+#while true
+#        do
+#            read -p " 请输入key path：" key_path
+#            if [ !  -f "${key_path}" ]; then
+#                echoColor red " key path wrong，请重新输入！"
+#				echoColor green "请输入证书key文件路径(需fullchain):"
+#            else
+#                break
+#            fi
+#        done
+#
+#
+#
 
 
 
