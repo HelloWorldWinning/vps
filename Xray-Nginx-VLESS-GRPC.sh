2f02612449b3fb49927172d225df9683607203ea
HelloWorldWinning <feedzhulei@aol.com>
2022-12-08 00:04:40 +0800

    arc 64

--- Xray-Nginx-VLESS-GRPC.sh
+++ Xray-Nginx-VLESS-GRPC.sh
@@ -368,7 +368,7 @@ temp_d=$(mktemp -d)
 
          
     elif [ $get_arch = "aarch64" ];then
-        wget -q -O $temp_f  --no-check-certificate https://github.com/XTLS/xray-core/releases/download/${version}/xray-linux-64.zip
+        wget -q -O $temp_f  --no-check-certificate https://github.com/XTLS/xray-core/releases/download/${version}/Xray-linux-arm64-v8a.zip
         unzip $temp_f -d $temp_d/
         mv -fv $temp_d/xray /usr/bin/xrayR
         mv -fv $temp_d/* /usr/bin/
