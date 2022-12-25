83ba37325a6c0035dd44a8a177691066af55e93f
root <feedzhulei@aol.com>
2022-12-25 13:04:02 -0500

    Sun 25 Dec 2022 01:04:02 PM EST

--- new_nginx_conf.sh
+++ new_nginx_conf.sh
@@ -158,7 +158,7 @@ read -p "port  default: 9988: " Port
 
 
 
-cat <<EOF > /etc/nginx/conf.d/nginx_$Port.conf
+cat <<EOF > /etc/nginx/conf.d/${Port}.conf
 server {
     listen $Port;
     listen [::]:$Port;
