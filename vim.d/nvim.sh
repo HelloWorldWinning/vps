c7cee3292345cbb410a1a87b4834fde66a6bd06a
root <feedzhulei@aol.com>
2023-01-15 01:38:54 -0500

    Sun 15 Jan 2023 12:50:59 AM EST

---
+++ vim.d/nvim.sh
@@ -0,0 +1,85 @@
+# https://jdhao.github.io/2018/09/05/centos_nvim_install_use_guide/ Linux 下 Neovim 配置 Python 开发环境指南
+# https://juejin.cn/post/6844904118948118536 如何将你的 neovim 打造成 vscode 一般的 Python IDE?
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
+apt-get update
+apt-get install -y nodejs
+apt install -y node 
+
+pip install --upgrade pip
+pip install neovim  pynvim jedi
+
+
+mkdir  -p ~/.config/nvim
+
+cat>~/.config/nvim/init.vim<<EOF
+let g:deoplete#enable_at_startup = 1
+let g:coc_disable_startup_warning = 1
+
+call plug#begin('~/.local/share/nvim/plugged')
+
+Plug 'morhetz/gruvbox'
+colorshceme gruvbox    
+
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'zchee/deoplete-jedi'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'vim-airline/vim-airline'
+Plug 'jiangmiao/auto-pairs'
+Plug 'christoomey/vim-tmux-navigator'
+
+Plug 'preservim/nerdtree'
+Plug 'preservim/nerdcommenter' 
+
+Plug 'vim-airline/vim-airline'
+let g:airline_theme='badwolf'  "可以自定义主题，这里使用 badwolf
+Plug 'kkoomen/vim-doge'
+Plug 'preservim/nerdcommenter' 
+
+call plug#end()
+
+
+
+
+syntax on
+filetype plugin on
+"set showtabline=2
+set tabline=%F\ %y 
+set number 
+set relativenumber
+set ruler
+"set rulerformat=%l/[%L]:%v
+"let g:rulerformat_filepath_format = '%f'
+
+
+
+EOF
+
+
+
+#wget -O ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+#dpkg -i ripgrep.deb
+
+
+cat >>~/.bashrc<<EOF
+alias n='nvim'
+alias _g='git add . && git commit -m   " `date` " && git push'
+EOF
+
+source ~/.bashrc
+
+
+cat  <<- EOF
+
+:CocInstall coc-python
+:CocInstall coc-snippets
+:CocInstall coc-bookmark
+:PlugInstall
+
+EOF
+
+
+
