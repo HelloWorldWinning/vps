d7d92161ab23549f08cbc6ae45f7619843cada05
root <feedzhulei@aol.com>
2023-02-20 11:32:44 -0500

    Sun 15 Jan 2023 05:27:01 PM CST

--- Xray-Nginx-VLESS-GRPC_docker.sh
+++ Xray-Nginx-VLESS-GRPC_docker.sh
@@ -1,3 +1,26 @@
+
+ports_to_one(){
+apt-get install iptables-persistent -y
+
+net_card=$(ip addr |grep BROADCAST|head -1|awk '{print $2; exit}'|cut -d ":" -f 1)
+
+iptables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :${Port}
+
+ip6tables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :${Port}
+
+
+iptables-save -f /etc/iptables/rules.v4
+ip6tables-save -f /etc/iptables/rules.v6
+
+
+crontab -l > conf && echo  -e "@reboot sleep 13; /usr/sbin/iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
+
+crontab -l > conf && echo  -e "@reboot sleep 14; /usr/sbin/ip6tables-restore < /etc/iptables/rules.v6" >> conf && crontab conf && rm -f conf
+
+}
+
+
+
 result=$(echo $(docker ps -a |cut -d" " -f1)|grep CONTAINER)
     if [ -z "$result" ]; then
 
@@ -158,9 +181,14 @@ mkdir -p /etc/xrayR/
 
 Get_Key_Path
 
-read -p "port  default: 22280: " Port
+#read -p "port  default: 22280: " Port
+#    if   [[ -z "$Port" ]]; then
+#            Port=22280
+#
+#   fi
+read -p "port  default: 65503: " Port
     if   [[ -z "$Port" ]]; then
-            Port=22280
+            Port=65503
 
     fi
 
@@ -518,6 +546,8 @@ read -p " 选择：" answer
             Xray_Grpc_Nginx
             DownloadxrayRCore
             start_docker
+ports_to_one
+
     #     Restart_Ng_under_links
 	    #get_nginx_port
 #netstat -ltnp  | grep $nginx_port
--- xray_mianliu.sh
+++ xray_mianliu.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 # xray一键安装脚本
 # Author: hijk<https://hijk.art>
-
+#############
+#ports_to_one(){
+#apt-get install iptables-persistent -y
+#
+#net_card=$(ip addr |grep BROADCAST|head -1|awk '{print $2; exit}'|cut -d ":" -f 1)
+#
+#iptables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :65503
+#
+#ip6tables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :65503
+#
+#
+#iptables-save -f /etc/iptables/rules.v4
+#ip6tables-save -f /etc/iptables/rules.v6
+#
+#
+#crontab -l > conf && echo  -e "@reboot sleep 13; /usr/sbin/iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
+#
+#crontab -l > conf && echo  -e "@reboot sleep 14; /usr/sbin/ip6tables-restore < /etc/iptables/rules.v6" >> conf && crontab conf && rm -f conf
+#}
+#######
 
 apt install dnsutils vim unzip  jq  net-tools  -y
 
