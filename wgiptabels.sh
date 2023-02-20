d7cd154bb3222af21b0b2e53e471f8be70cf2ae5
root <feedzhulei@aol.com>
2023-02-20 00:30:04 -0500

    Sun 15 Jan 2023 05:27:01 PM CST

--- wgiptabels.sh
+++ wgiptabels.sh
@@ -3,8 +3,8 @@ iptables-save -f /etc/iptables/rules.v4
 ip6tables-save -f /etc/iptables/rules.v6
 
 
-crontab -l > conf && echo  -e "@reboot iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
+crontab -l > conf && echo  -e "@reboot sleep 10; /usr/sbin/iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
 
-crontab -l > conf && echo  -e "@reboot ip6tables-restore < /etc/iptables/rules.v6" >> conf && crontab conf && rm -f conf
+crontab -l > conf && echo  -e "@reboot sleep 11; /usr/sbin/ip6tables-restore < /etc/iptables/rules.v6" >> conf && crontab conf && rm -f conf
 
 
