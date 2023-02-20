a74152401125a6f4652dd1461ee8e26339c1af87
root <feedzhulei@aol.com>
2023-02-20 00:34:56 -0500

    Sun 15 Jan 2023 05:27:01 PM CST

--- wgiptabels.sh
+++ wgiptabels.sh
@@ -1,4 +1,11 @@
 apt-get install iptables-persistent -y
+
+
+iptables -t nat -A PREROUTING -i eth0 -p udp --dport 55000:59999 -j DNAT --to-destination :65503
+
+ip6tables -t nat -A PREROUTING -i eth0 -p udp --dport 55000:59999 -j DNAT --to-destination :65503
+
+
 iptables-save -f /etc/iptables/rules.v4
 ip6tables-save -f /etc/iptables/rules.v6
 
