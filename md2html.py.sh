3e9b9c25cae60a3b1ea43bbe3200a50991f52d4c
root <feedzhulei@aol.com>
2023-03-21 19:39:22 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- md2html.py.sh
+++ md2html.py.sh
@@ -1,2 +1,2 @@
-wget -4 -O  md2html.py.sh  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/md2html.py 
+wget -4 -O  md2html.py  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/md2html.py 
 python md2html.py
