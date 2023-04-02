73d0c7ddf31ae32690345fc49a71a85f4cb7aa0c
root <feedzhulei@aol.com>
2023-04-02 11:20:05 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- ssl.sh
+++ ssl.sh
@@ -6,11 +6,6 @@ YELLOW="\033[33m"   # Warning message
 BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'
 
-#jq_not=$(apt --installed list | grep jq |wc -l)
-#if [  $jq_not=0 ];then
-# apt install jq -y
-#fi
-
 
 
 Acme_Get(){
@@ -18,168 +13,34 @@ Acme_Get(){
 curl -4  -sL https://get.acme.sh | sh -s email=hijk.pw@protonmail.ch
 source ~/.bashrc
 ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh   --issue -d $Domain --keylength ec-256 --force  --standalone --listen-v6
-
-}
-
-Get_Key_Path(){
-  
-read -p "请输入域名: " Domain
-cer_path=/root/.acme.sh/${Domain}/${Domain}.cer
-key_path=/root/.acme.sh/${Domain}/${Domain}.key
-
-if [[ -f $cer_path ]]  && [[ -f $key_path ]]  ; then
-echo $cer_path
-echo $key_path
-
-else
-
-Acme_Get
-
-cer_path=/root/.acme.sh/${Domain}/${Domain}.cer
-key_path=/root/.acme.sh/${Domain}/${Domain}.key
-echo $cer_path
-echo $key_path
-
-fi
+#~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+#~/.acme.sh/acme.sh   --issue -d $Domain --keylength 2048 --force  --standalone --listen-v6
 
 }
 
 
+get_ssl{
+
+read  -p "input Domain" Domain
+~/.acme.sh/acme.sh   --issue -d $Domain --keylength 2048    --standalone --listen-v6
 
 
-
-getData() {
-    if [[ "$TLS" = "true" || "$XTLS" = "true" ]]; then
-        echo ""
-        echo " Xray一键脚本，运行之前请确认如下条件已经具备："
-        colorEcho ${YELLOW} "  1. 伪装域名DNS解析指向当前服务器ip$IPV4$IPV6 "
-        echo " "
-#        read -p " 确认满足按y，按其他退出脚本：" answer
-#        if [[ "${answer,,}" != "y" ]]; then
-#            exit 0
-#        fi
-#
-        echo ""
-        while true
-        do
-            read -p "请输入伪装域名: " DOMAIN
-            if [[ -z "${DOMAIN}" ]]; then
-                colorEcho ${RED} " 域名输入错误，请重新输入！"
-            else
-                break
-            fi
-        done
-        DOMAIN=${DOMAIN,,}
-        colorEcho ${BLUE}  "伪装域名(host): $DOMAIN"
-
-        echo ""
-        if [[ -f ~/xray.pem && -f ~/xray.key ]]; then
-            colorEcho ${BLUE}  " 检测到自有证书，将使用其部署"
-            CERT_FILE="/usr/local/etc/xray/${DOMAIN}.pem"
-            KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
-        else
-#            resolve=`curl -4  -sL ipget.net/?ip=${DOMAIN}`
-#	    resolve="dig +short ${DOMAIN} @1.1.1.1"	    
-#		resolve="$(dig A  +short ${DOMAIN} @1.1.1.1)"
-
-IPV4=$(dig @1.1.1.1 +short  txt ch  whoami.cloudflare  |tr -d \")
-IPV6=$(dig +short @2606:4700:4700::1111 -6 ch txt whoami.cloudflare|tr -d \")
-resolve4="$(dig A  +short ${DOMAIN} @1.1.1.1)"
-resolve6="$(dig AAAA +short ${DOMAIN} @1.1.1.1)"
-res4=`echo -n ${resolve4} | grep $IPV4`
-res6=`echo -n ${resolve6} | grep $IPV6`
-res=`echo $res4$res6`
-IP=`echo $res4$res6`
-echo "${DOMAIN}  points to: $res"
-
-if [[ -z "${res}" ]]; then
-echo " ${DOMAIN} 解析结果：${res}"
-echo -e " ${RED}伪装域名未解析到当前服务器 $IPV4 $IPV6 "
-exit 1
-fi
 }
-
-getCert() {
-    if [[ -z ${CERT_FILE+x} ]]; then
-        stopNginx
-        systemctl stop xray
-        res=`netstat -ntlp| grep -E ':80 |:443 '`
-        if [[ "${res}" != "" ]]; then
-            colorEcho ${RED}  " 其他进程占用了80或443端口，请先关闭再运行一键脚本"
-            echo " 端口占用信息如下："
-            echo ${res}
-            exit 1
-        fi
-
-        $CMD_INSTALL socat openssl
-        if [[ "$PMT" = "yum" ]]; then
-            $CMD_INSTALL cronie
-            systemctl start crond
-            systemctl enable crond
-        else
-            $CMD_INSTALL cron
-            systemctl start cron
-            systemctl enable cron
-        fi
-        curl -4  -sL https://get.acme.sh | sh -s email=hijk.pw@protonmail.sh
-        source ~/.bashrc
-        ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
-        ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-        if [[ "$BT" = "false" ]]; then
-			if [[ ! -z "${res4}" ]]; then
-            ~/.acme.sh/acme.sh   --issue -d $DOMAIN --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx"  --standalone --listen-v6
-			else
-            ~/.acme.sh/acme.sh   --issue -d $DOMAIN --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx"  --standalone --listen-v6
-			fi				
-        else
-			if [[ ! -z "${res4}" ]]; then
-            ~/.acme.sh/acme.sh   --issue -d $DOMAIN --keylength ec-256 --pre-hook "nginx -s stop || { echo -n ''; }" --post-hook "nginx -c /www/server/nginx/conf/nginx.conf || { echo -n ''; }"  --standalone --listen-v6
-			else
-            ~/.acme.sh/acme.sh   --issue -d $DOMAIN --keylength ec-256 --pre-hook "nginx -s stop || { echo -n ''; }" --post-hook "nginx -c /www/server/nginx/conf/nginx.conf || { echo -n ''; }"  --standalone --listen-v6
-			fi
-        fi
-        [[ -f ~/.acme.sh/${DOMAIN}/ca.cer ]] || {
-            colorEcho $RED " 获取证书失败，请复制上面的红色文字到 https://hijk.art 反馈"
-            exit 1
-        }
-        CERT_FILE="/usr/local/etc/xray/${DOMAIN}.pem"
-        KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
-        ~/.acme.sh/acme.sh  --install-cert -d $DOMAIN --ecc \
-            --key-file       $KEY_FILE  \
-            --fullchain-file $CERT_FILE \
-            --reloadcmd     "service nginx force-reload"
-        [[ -f $CERT_FILE && -f $KEY_FILE ]] || {
-            colorEcho $RED " 获取证书失败，请到 https://hijk.art 反馈"
-            exit 1
-        }
-    else
-        cp ~/xray.pem /usr/local/etc/xray/${DOMAIN}.pem
-        cp ~/xray.key /usr/local/etc/xray/${DOMAIN}.key
-    fi
-}
-
-
-
-
 
 menu() {
     clear
     echo "#############################################################"
-    echo -e "  ${GREEN}1.${PLAIN}   安装Xray-VMESS"
-    echo -n " 当前状态："
-    statusText
+    echo  "1 获取ssl file "
     echo 
 
-    read -p " 请选择操作[0-17]：" answer
+    read -p " 请选择操作：" answer
     case $answer in
         0)
             exit 0
             ;;
         1)
-            ;;
-        2)
+		Acme_Get
+		get_ssl
             ;;
         *)
             colorEcho $RED " 请选择正确的操作！"
@@ -188,16 +49,3 @@ menu() {
     esac
 }
 
-checkSystem
-
-action=$1
-[[ -z $1 ]] && action=menu
-case "$action" in
-    menu|update|uninstall|start|restart|stop|showInfo|showLog)
-        ${action}
-        ;;
-    *)
-        echo " 参数错误"
-        echo " 用法: `basename $0` [menu|update|uninstall|start|restart|stop|showInfo|showLog]"
-        ;;
-esac
