d200b9f88a313b5504595ec8f66f9c1ce65287d2
root <feedzhulei@aol.com>
2023-04-22 11:11:41 -0400

    Sun 15 Jan 2023 05:27:01 PM CST

--- new_nginx_conf.sh
+++ new_nginx_conf.sh
@@ -61,7 +61,7 @@ curl -sL https://get.acme.sh | sh -s email=hijk.pw@protonmail.ch
 source ~/.bashrc
 ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh   --issue -d $Domain --keylength ec-256 --force  --standalone --listen-v6
+~/.acme.sh/acme.sh   --issue -d $Domain --keylength ec-256 --force  --standalone --listen-v6 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx" 
 
 }
 
