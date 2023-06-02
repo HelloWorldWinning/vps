fe4f9b3d2e604cdabacdc4f501cd4a69f66aae6e
root <feedzhulei@aol.com>
2023-06-02 10:26:09 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- tcp131721.sh
+++ tcp131721.sh
@@ -23,12 +23,13 @@ alias c='clear'
 alias l='ls -lrth'
 alias s='ls -lhSr'
 alias nm='ls -lh'
+alias ln='ls -lh'
 alias p='python'
 alias _GP='git  pull'
 alias _G='git add . && git commit -m   " Sun 15 Jan 2023 05:27:01 PM CST " && git push ;echo " ";date;echo " "'
 alias _F='git pull && git add . && git commit -m   " Sun 15 Jan 2023 05:27:01 PM CST " && git push ;echo " ";date;echo " "'
 alias n='/usr/bin/nvim.appimage'
-alias _ai='docker ps --format "{{.Names}}" |grep  "code_love_bot\|Codex_openai_bot\|openAI_Smart_Wisdom\|text_davinci_003_high_bot\|text_davinci_003_low_bot" |xargs -I {} docker restart {}'
+#alias _ai='docker ps --format "{{.Names}}" |grep  "code_love_bot\|Codex_openai_bot\|openAI_Smart_Wisdom\|text_davinci_003_high_bot\|text_davinci_003_low_bot" |xargs -I {} docker restart {}'
 export OPENAI_API_KEY=${OPENAI_API_KEY}
 EOF
 
