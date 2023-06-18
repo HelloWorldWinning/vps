fd7ed03d1fa2e7cd83e05710acd43b2f27155587
root <stone100100@gmail.com>
2023-06-18 15:35:26 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- wgiptabels.sh
+++ wgiptabels.sh
@@ -7,7 +7,7 @@ iptables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT 
 ip6tables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT --to-destination :65503
 
 
-iptables-save -f /etc/iptables/rules.v4
+iptables-save -f /etc/iptables/rules.v4 
 ip6tables-save -f /etc/iptables/rules.v6
 
 
