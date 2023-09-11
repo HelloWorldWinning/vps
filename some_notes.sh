52610efde9d958c2fd59efcbdbea4a5259407803
HelloWorldWinning <feedzhulei@aol.com>
2023-09-11 08:01:01 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

---
+++ some_notes.sh
@@ -0,0 +1,5 @@
+
+先wgcf 后 wg
+
+PostUp =   iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o wgcf -j MASQUERADE; ip6tables -A FORWARD -i %i -j ACCEPT; ip6tables -t nat -A POSTROUTING -o wgcf -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o wgcf -j MASQUERADE; ip6tables -D FORWARD -i %i -j ACCEPT; ip6tables -t nat -D POSTROUTING -o wgcf -j MASQUERADE
