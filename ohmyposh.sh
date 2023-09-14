9ea043e3b589f256dcf14c157458bef64ca00431
HelloWorldWinning <feedzhulei@aol.com>
2023-09-14 15:09:09 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- ohmyposh.sh
+++ ohmyposh.sh
@@ -1,4 +1,5 @@
 curl -4s https://ohmyposh.dev/install.sh | bash -s
+mkdir -p /root/themes/
 wget   --inet4-only  -O  /root/themes/gmay3.omp.json https://raw.githubusercontent.com/HelloWorldWinning/vps/main/gmay3.omp.json  
 cat >>~/.bashrc<<EOF
 eval "\$(oh-my-posh init bash --config /root/themes/gmay3.omp.json)"
