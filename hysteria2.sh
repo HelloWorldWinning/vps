dd14cc5f740dc727b6e5eba1b47006e8bb40c8ac
HelloWorldWinning <feedzhulei@aol.com>
2023-09-16 23:57:11 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- hysteria2.sh
+++ hysteria2.sh
@@ -1,6 +1,35 @@
+#echo "Please enter a domain:"
+#read domain
+#echo "You entered: $domain"
+#
+
+# Identify the primary network interface (regardless of wg status)
+PRIMARY_NETWORK_INTERFACE=$(ip route | grep default | awk '{print $5}')
+
+# Get the authentic public IP by querying Cloudflare's DNS server 1.1.1.1
+AUTHENTIC_PUBLIC_IP=$(dig @1.1.1.1 whoami.cloudflare ch txt +short -b $(ip -4 addr show $PRIMARY_NETWORK_INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}') | tr -d '"')
+
+# Read user input for domain name
 echo "Please enter a domain:"
-read domain
-echo "You entered: $domain"
+read DOMAIN_INPUT
+echo "You entered:"
+echo "$DOMAIN_INPUT"
+
+# Get the IP address of the entered domain
+DOMAIN_IP=$(dig @1.1.1.1 $DOMAIN_INPUT +short)
+
+# Check if the DOMAIN_IP matches the AUTHENTIC_PUBLIC_IP
+if [ "$DOMAIN_IP" = "$AUTHENTIC_PUBLIC_IP" ]; then
+#   echo $DOMAIN_INPUT
+    echo $AUTHENTIC_PUBLIC_IP
+    echo "The domain IP matches the authentic public IP."
+else
+    echo "The domain IP does not match the authentic public IP."
+    exit 1
+
+fi
+
+
 
 
 bash <(curl -fsSL https://get.hy2.sh/)
