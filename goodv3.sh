e49e9d8e850b2bca34ba3d2efc518e0971ebafac
HelloWorldWinning <feedzhulei@aol.com>
2023-09-16 14:34:50 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- goodv3.sh
+++ goodv3.sh
@@ -535,6 +535,7 @@ ${Red_font_prefix}87${Font_color_suffix} prefer_ipv4
 ${Red_font_prefix}88${Font_color_suffix} get_fraud_score
 ${Red_font_prefix}89${Font_color_suffix} some notes
 ${Red_font_prefix}90${Font_color_suffix} ohmyposh.sh
+${Red_font_prefix}91${Font_color_suffix} hysteria2.sh
  
 
 
@@ -721,6 +722,7 @@ nohup ./AWS-Panel-linux-amd64 > /dev/null 2>&1 &
                 88) option_88_function ;;
 		89) curl  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/some_notes.sh  ;;
 		90) bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ohmyposh.sh  ) ;;
+		91) bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/hysteria2.sh ) ;;
 
 		00)eval "exit";;
 		q)eval "exit";;
