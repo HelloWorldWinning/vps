8e5ee26a8e883c49036076ebd4d1795e77574ca4
HelloWorldWinning <feedzhulei@aol.com>
2023-10-05 00:12:22 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- some_notes.sh
+++ some_notes.sh
@@ -18,6 +18,9 @@
 ================================================================
 ================================================================
 ================================================================
+Private key: YAjoKYIZ601zDTrYJKGoibA0bNTKCboCJNGUH7wgdn4
+Public key: N9IY9bJiPgpe_1exP9LGkNHhqmbBL4tDbXc0lQEr9z8
+
 https://github.com/zxcvos/Xray-script
 Xray-REALITY 管理脚本
 wget --no-check-certificate -O ${HOME}/Xray-script.sh https://raw.githubusercontent.com/zxcvos/Xray-script/main/reality.sh && bash ${HOME}/Xray-script.sh
