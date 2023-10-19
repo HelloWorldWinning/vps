a9fa68aac7102ef4760abd5956ef409677ff2235
HelloWorldWinning <feedzhulei@aol.com>
2023-10-19 14:03:22 +0800

    Sun 15 Jan 2023 05:27:01 PM CST

--- ohmyposh.sh
+++ ohmyposh.sh
@@ -1,3 +1,5 @@
+apt install  -y unzip
+
 curl -4s https://ohmyposh.dev/install.sh | bash -s
 mkdir -p /root/themes/
 wget   --inet4-only  -O  /root/themes/gmay3.omp.json https://raw.githubusercontent.com/HelloWorldWinning/vps/main/gmay3.omp.json  
