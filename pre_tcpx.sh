b31732d56ab8982a085e669818c35747e82e21c4
HelloWorldWinning <feedzhulei@aol.com>
2023-10-26 23:03:48 +0800

    Mon Oct 23 06:50:12 PM CST 2023

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -43,7 +43,8 @@ alias dc='docker-compose'
 
 bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/sys_info.sh  )
 
-bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ip_check2_simple.sh  ) </dev/null
+#bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ip_check2_simple.sh  ) </dev/null
+bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ip_check2.sh  ) </dev/null
 echo ""
 l
 EOF
