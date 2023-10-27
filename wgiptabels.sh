a9fac80da883ae96049e2789dce7d7acea65485a
HelloWorldWinning <feedzhulei@aol.com>
2023-10-28 03:18:27 +0800

    Mon Oct 23 06:50:12 PM CST 2023

--- wgiptabels.sh
+++ wgiptabels.sh
@@ -7,8 +7,13 @@ iptables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT 
 ip6tables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT --to-destination :65503
 
 
-iptables-save -f /etc/iptables/rules.v4 
-ip6tables-save -f /etc/iptables/rules.v6
+iptables-save > /etc/iptables/rules.v4
+ip6tables-save > /etc/iptables/rules.v6
+
+
+
+#iptables-save -f /etc/iptables/rules.v4 
+#ip6tables-save -f /etc/iptables/rules.v6
 
 
 crontab -l > conf && echo  -e "@reboot sleep 13; /usr/sbin/iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
