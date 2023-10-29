367aefa95b28ee029f0b8887c1c1ef1dd87a5703
HelloWorldWinning <feedzhulei@aol.com>
2023-10-29 12:40:44 +0800

    Mon Oct 23 06:50:12 PM CST 2023

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -38,7 +38,8 @@ alias txl='tmux list-sessions'
 alias txlw='tmux list-windows '
 
 alias dc='docker-compose'
-alias cc='bash  <(curl -Ls4  bit.ly/myvpsjingyi)'
+#alias cc='bash  <(curl -Ls4  bit.ly/myvpsjingyi)'
+alias cc='bash  <(curl -Ls4   https://raw.githubusercontent.com/HelloWorldWinning/vps/main/goodv3.sh   )'
 
 
 bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/sys_info.sh  )
