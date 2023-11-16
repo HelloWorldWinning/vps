bf6b033ffcef710b370398d703edb2451be2471f
HelloWorldWinning <feedzhulei@aol.com>
2023-11-16 15:01:31 +0800

    Sun Nov  5 01:35:22 PM CST 2023

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -4,7 +4,7 @@ apt-get update
 echo "nameserver 8.8.8.8" |  tee -a /etc/resolv.conf
 echo "nameserver 8.8.4.4" |  tee -a /etc/resolv.conf
 
-apt-get install -y xsel  xclip git poppler-utils
+apt-get install -y xsel  xclip git poppler-utils calcurse
 
 git config --global core.editor "vim"
 
