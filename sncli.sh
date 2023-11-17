fc1de155a3c489d39e0c09ccb5f8b4d5bc461990
HelloWorldWinning <feedzhulei@aol.com>
2023-11-17 15:16:20 +0800

    Sun Nov  5 01:35:22 PM CST 2023

--- sncli.sh
+++ sncli.sh
@@ -2,21 +2,21 @@ pip3 install sncli
 cat >>~/.bashrc<<EOF
 alias sn='sncli'
 EOF
-cat >~/.snclirci<<EOF
+cat >~/.snclirc<<EOF
 [sncli]
 cfg_sn_username = 
 cfg_sn_password = 
 cfg_sn_password_eval = gpg --quiet --for-your-eyes-only --no-tty --decrypt ~/.sncli-pass.gpg
 #kb_edit_note =  enter
 #kb_page_down =  enter f
-kb_page_up =    command e
+#kb_page_up =    command e
 clr_note_focus_bg = light blue
 cfg_editor = vim
 
 # help https://sncli.readthedocs.io/_/downloads/en/latest/pdf/
 # https://github.com/insanum/sncli
 EOF
-
+echo ""
 echo "sncli.sh run successfully: sn to use"
 
 
