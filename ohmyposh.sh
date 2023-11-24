592c1ea7bac236b4c5bdda9268068aa8e42dd04a
HelloWorldWinning <feedzhulei@aol.com>
2023-11-24 12:46:19 +0800

    Fri Nov 24 12:46:19 PM CST 2023

--- ohmyposh.sh
+++ ohmyposh.sh
@@ -14,7 +14,7 @@ apt install  -y unzip
 
 curl -4s https://ohmyposh.dev/install.sh | bash -s
 mkdir -p /root/themes/
-wget   --inet4-only  -O  /root/themes/gmay3.omp.json https://raw.githubusercontent.com/HelloWorldWinning/vps/main/gmay3.omp.json  
+wget   --inet4-only  -O  ~/themes/gmay3.omp.json https://raw.githubusercontent.com/HelloWorldWinning/vps/main/gmay3.omp.json  
 cat >>~/.bashrc<<EOF
 export country_code=\$(curl -s 'https://ipinfo.io/json?token=6d89f8e7f1a21e' | grep '\"country\":' | awk -F'\"' '{print \$4}')
 export weather_temperature=\$(curl -s wttr.in/shapingba?format="%t,%C" | sed 's/+//')
