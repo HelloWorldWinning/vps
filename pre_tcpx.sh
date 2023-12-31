1531d3c9a2c616e288d9c9c09a7f9a6cf0c6f826
HelloWorldWinning <feedzhulei@aol.com>
2023-12-31 16:10:22 +0800

    Sun Dec 31 04:10:22 PM CST 2023

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -47,7 +47,7 @@ html_content=\$(curl -m 10 -s 'http://www.weather.com.cn/weather/101040100.shtml
 weather=\$(echo "\$html_content" |   grep -oP '(?<=class="wea">).*?(?=</p>)' |head -n2 | tr '\n' ';' | sed 's/;\$//'  )
 temperature=\$(echo "\$html_content" | grep -oP '(?<=<i>).*?(?=℃</i>)' |head -n 1 ) 
 we_temp="\${temperature}°C \${weather}"
-export weather_temperature=$we_temp
+weather_temperature="\${temperature}°C \${weather}"
 
 alias we='curl -m 6  wttr.in/shapingba'
 alias ca='calcurse'
