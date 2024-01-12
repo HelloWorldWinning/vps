e28e4d5fe9ea221ba5aaba847aafeb10167e0c51
HelloWorldWinning <feedzhulei@aol.com>
2024-01-12 16:31:17 +0800

    Fri Jan 12 04:31:17 PM CST 2024

--- ft.sh
+++ ft.sh
@@ -50,13 +50,6 @@ curl https://raw.githubusercontent.com/freqtrade/freqtrade/stable/docker-compose
 sed -i "s/127.0.0.1:8080:8080/${port}:8080/" docker-compose.yml
 sed -i "s/container_name: freqtrade/container_name: ${folder_name}_${port}/" docker-compose.yml
 
-
-# Pull the freqtrade image
-docker-compose pull
-
-# Create user directory structure
-docker-compose run --rm freqtrade create-userdir --userdir user_data
-
 # Create configuration - Requires answering interactive questions
 #docker-compose run --rm freqtrade new-config --config user_data/config.json
 
@@ -104,6 +97,13 @@ case $choice in
 esac
 
 
+
+
+# Pull the freqtrade image
+docker-compose pull
+
+# Create user directory structure
+docker-compose run --rm freqtrade create-userdir --userdir user_data
 
 
 
