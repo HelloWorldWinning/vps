31d6704109759d4361ee39e902d9845f602004b3
HelloWorldWinning <feedzhulei@aol.com>
2024-01-28 22:51:03 +0800

    Sun Jan 28 10:51:03 PM CST 2024

---
+++ hostname_length_adjuster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Path to your JSON configuration
+config_path="/root/themes/gmay3.omp.json"
+
+# Get the actual hostname to calculate its length
+hostname=$(hostname)
+hostname_length=${#hostname}
+
+# Generate a string of spaces equal to the length of the hostname
+spaces=$(printf '%*s' $hostname_length)
+
+# Update the template in the JSON file
+# This requires jq - a command-line JSON processor
+jq '.blocks[1].segments[0].template = "'"$spaces        \uf04b"'"' $config_path > temp.json && mv temp.json $config_path
+
+# Now you can initialize Oh-My-Posh
+eval "$(oh-my-posh init bash --config $config_path)"
