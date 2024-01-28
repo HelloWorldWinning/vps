f9555eb8fd7708c2c2e117cd994ceb4884f01573
HelloWorldWinning <feedzhulei@aol.com>
2024-01-29 01:13:01 +0800

    Mon Jan 29 01:13:01 AM CST 2024

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-#top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%\n", usage}'
-#cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
-#ram=$(free | grep Mem | awk '{printf "%.0f%%", $3/$2 * 100}')
-#echo "$cpu $ram"
-
-#!/bin/bash
-#cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
-#ram=$(free | grep Mem | awk '{printf "%.0f%%", $3/$2 * 100}')
-#disk=$(df -h | grep '/$' | awk '{print $5}')
-
-cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f", usage}')
-ram=$(free | grep Mem | awk '{printf "%.0f", $3/$2 * 100}')
-#disk=$(df -h | grep '/$' | awk '{print $5}')
+cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
+ram=$(free -h | grep Mem | awk '{sub("i", "", $7); print $7}')
 disk=$(df -h | grep '/$' | awk '{print $4}')
+
 echo "$cpu $ram $disk"
+
