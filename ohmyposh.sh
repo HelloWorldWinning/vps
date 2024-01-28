d0e78a936ba522e17b833b0b5726e34cd95b6c20
HelloWorldWinning <feedzhulei@aol.com>
2024-01-29 00:54:13 +0800

    Mon Jan 29 12:54:13 AM CST 2024

--- ohmyposh.sh
+++ ohmyposh.sh
@@ -5,7 +5,11 @@ clear
 curl -4s https://ohmyposh.dev/install.sh | bash -s
 mkdir -p ~/themes/
 wget   --inet4-only  -O  ~/themes/gmay3.omp.json https://raw.githubusercontent.com/HelloWorldWinning/vps/main/gmay3.omp.json  
+
 wget   --inet4-only  -O  ~/themes/hostname_length_adjuster.sh https://raw.githubusercontent.com/HelloWorldWinning/vps/main/hostname_length_adjuster.sh
+
+wget   --inet4-only  -O  ~/themes/cpu_usage.sh  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/hostname_length_adjuster.sh
+
 cat >>~/.bashrc<<EOF
 export country_code=\$(curl -s 'https://ipinfo.io/json?token=6d89f8e7f1a21e' | grep '\"country\":' | awk -F'\"' '{print \$4}')
 bash  ~/themes/hostname_length_adjuster.sh
