102cc2f4b78c11ece91f71deff786b9a625e1f91
HelloWorldWinning <feedzhulei@aol.com>
2024-01-30 14:03:41 +0800

    Tue Jan 30 02:03:41 PM CST 2024

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
+#cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
+cpu=$(top -bn1 | grep "Cpu(s)" | awk '{idle=$8; printf "%.0f%%", idle}')
 ram=$(free -h | grep Mem | awk '{sub("i", "", $7); print $7}')
 disk=$(df -h | grep '/$' | awk '{print $4}')
 
