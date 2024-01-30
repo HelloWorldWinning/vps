384fb8d64cae15a7d47f47fd1bfa3117aad9ac4e
HelloWorldWinning <feedzhulei@aol.com>
2024-01-30 15:06:52 +0800

    Tue Jan 30 03:06:52 PM CST 2024

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 #cpu=$(top -bn1 | grep "Cpu(s)" | awk '{usage=$2+$4; printf "%.0f%%", usage}')
-cpu=$(top -bn1 | grep "Cpu(s)" | awk '{idle=$8; printf "%.0f%%", idle}')
+#cpu=$(top -bn1 | grep "Cpu(s)" | awk '{idle=$8; printf "%.0f%%", idle}')
+
+
+#!/bin/bash
+
+# Read the first line from /proc/stat
+read -r cpu user nice system idle iowait irq softirq steal _ < /proc/stat
+
+# Calculate total and idle times
+total_time=$((user + nice + system + idle + iowait + irq + softirq + steal))
+idle_time=$((idle + iowait))
+
+# Calculate idle CPU percentage
+idle_cpu=$((100 * (idle_time) / total_time))
+
+#echo "${idle_cpu}%"
+
+
+
+
 ram=$(free -h | grep Mem | awk '{sub("i", "", $7); print $7}')
 disk=$(df -h | grep '/$' | awk '{print $4}')
 
-echo "$cpu $ram $disk"
+echo "$idle_cpu $ram $disk"
 
