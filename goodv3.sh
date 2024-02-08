fec339a6c0375038e37747bdfecf9cfebd3f97fb
HelloWorldWinning <feedzhulei@aol.com>
2024-02-09 01:02:27 +0800

    Fri Feb  9 01:02:27 AM CST 2024

--- goodv3.sh
+++ goodv3.sh
@@ -632,6 +632,7 @@ ${Red_font_prefix}101${Font_color_suffix} jupyter1666.sh
 ${Red_font_prefix}102${Font_color_suffix} 中文编码问题 txt
 ${Red_font_prefix}103${Font_color_suffix} bash  <(curl -sLk  yabs.sh  ) -i
 ${Red_font_prefix}104${Font_color_suffix} TA-Lib install
+${Red_font_prefix}105${Font_color_suffix} new branch.sh 
 ${Red_font_prefix}0${Font_color_suffix} ip checker
  
 
@@ -844,6 +845,7 @@ bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/
 		102) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/cn_coding.sh )  ;;
 		103) bash  <(curl -sLk  yabs.sh  ) -i ;;
 		104) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ta-lib.sh  )  ;;
+		105) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/branch.sh  )  ;;
 
 #	00)eval "exit";;
 		q)eval "exit";;
