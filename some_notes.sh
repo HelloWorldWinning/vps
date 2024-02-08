16753e8512a8fe5bc22b31cebb6f215e81043739
HelloWorldWinning <feedzhulei@aol.com>
2024-02-09 00:23:35 +0800

    Fri Feb  9 12:23:35 AM CST 2024

--- some_notes.sh
+++ some_notes.sh
@@ -162,6 +162,9 @@ sudo chmod 700 /root
 import openai
 import os
 
+#gpt-3.5-turbo  4,096 tokens
+#gpt-3.5-turbo-16k  16,385 tokens
+
 # Set the API key from the environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
