6bd224eb079c5288d710c308ed4c36ce4f427605
HelloWorldWinning <feedzhulei@aol.com>
2024-02-17 14:35:12 +0800

    Sat Feb 17 02:35:12 PM CST 2024

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -30,6 +30,23 @@ bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ip_
 mkdir -p /data
 
 cat >>~/.bashrc<<EOF
+###### _pre start
+
+#if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
+if true; then
+    # Check if a tmux session named "doing" exists
+    tmux has-session -t doing &>/dev/null
+    if [ \$? -ne 0 ]; then
+        # If the session does not exist, create it
+        tmux new-session -s doing -d
+    fi
+    # If we are not already inside a tmux session, attach to the "doing" session
+    if [ -z "\$TMUX" ]; then
+        tmux attach -t doing
+    fi
+fi
+
+
 
 # start pre tcp
 
@@ -107,7 +124,7 @@ cd /data
 echo ""
 l
 
-# end of pre tcp
+###### _pre end
 EOF
 
 source /root/.bashrc
