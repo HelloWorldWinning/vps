94e7a4bcb1663292cd7489759a34ed4632f71150
HelloWorldWinning <feedzhulei@aol.com>
2024-03-29 22:26:25 +0800

    Fri Mar 29 10:26:25 PM CST 2024

--- wgiptabels.sh
+++ wgiptabels.sh
@@ -6,9 +6,11 @@ iptables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT 
 
 ip6tables -t nat -A PREROUTING -i ${net_card} -p udp --dport 55000:60000 -j DNAT --to-destination :65503
 
-sh -c 'iptables-save > /etc/iptables/rules.v4'
-sh -c 'ip6tables-save > /etc/iptables/rules.v6'
+#sh -c 'iptables-save > /etc/iptables/rules.v4'
+#sh -c 'ip6tables-save > /etc/iptables/rules.v6'
 
+sh -c 'iptables-save > /etc/iptables/rules.v4' < /dev/null
+sh -c 'ip6tables-save > /etc/iptables/rules.v6' < /dev/null
 #iptables-save > /etc/iptables/rules.v4
 #ip6tables-save > /etc/iptables/rules.v6
 
@@ -19,5 +21,4 @@ sh -c 'ip6tables-save > /etc/iptables/rules.v6'
 crontab -l > conf && echo  -e "@reboot sleep 13; /usr/sbin/iptables-restore < /etc/iptables/rules.v4" >> conf && crontab conf && rm -f conf
 
 crontab -l > conf && echo  -e "@reboot sleep 14; /usr/sbin/ip6tables-restore < /etc/iptables/rules.v6" >> conf && crontab conf && rm -f conf
-
 
