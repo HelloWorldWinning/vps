e9e4043379f624b7f9293884eb65a5ab1e28b7ce
HelloWorldWinning <feedzhulei@aol.com>
2024-04-08 18:45:39 +0800

    Mon Apr  8 06:45:39 PM CST 2024

--- goodv3.sh
+++ goodv3.sh
@@ -863,8 +863,8 @@ bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/
 bash  <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/jupyter1666.sh )  
 bash  <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/jupyter166.sh ) 
 echo "======================================="
-        echo "sleep 5 seconds..."
-        sleep 5
+        echo "sleep 3 seconds..."
+        sleep 3
 	netstat -tulnp |grep 166	       
 	netstat -tulnp |grep 166 | wc -l	       
 			;;
