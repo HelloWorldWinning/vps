00f1008c3822a96b9a39010d3223a11aefe4c4e9
HelloWorldWinning <feedzhulei@aol.com>
2024-04-24 17:13:40 +0800

    Wed Apr 24 05:13:40 PM CST 2024

--- tmux_d/python_version.sh
+++ tmux_d/python_version.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-echo $(python_version=$(command -v python >/dev/null 2>&1 && python --version 2>&1 | awk     '{print $2}' || echo ''); conda_env=$(echo $CONDA_DEFAULT_ENV); if [ -z \"$conda_env\" ] && [ -z     \"$python_version\" ]; then echo ''; else echo $conda_env,$python_version; fi)
+echo $(python_version=$(command -v python >/dev/null 2>&1 && python --version 2>&1 | awk     '{print $2}' || echo ''); conda_env=$(echo $CONDA_DEFAULT_ENV); if [ -z \"$conda_env\" ] && [ -z     \"$python_version\" ]; then echo ''; else echo $conda_env $python_version; fi)
