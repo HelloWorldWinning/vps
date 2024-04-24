a9d06f06c9e7f8d42ac1ebdc9756cfc83d66491b
HelloWorldWinning <feedzhulei@aol.com>
2024-04-24 15:14:00 +0800

    Wed Apr 24 03:14:00 PM CST 2024

---
+++ tmux_d/tmux_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install tmux if not already installed
+sudo apt install -y tmux
+
+# Clone the oh-my-tmux repository
+git clone https://github.com/gpakosz/.tmux.git /data/.oh-my-tmux_d
+
+# Create the tmux configuration directory
+mkdir -p ~/.config/tmux
+
+# Create a symbolic link for the tmux configuration file
+#sudo ln -sf "$PWD/oh-my-tmux_d/.tmux.conf" ~/.config/tmux/tmux.conf
+sudo ln -sf  /data/.oh-my-tmux_d/.tmux.conf ~/.config/tmux/tmux.conf
+
+# Copy the local tmux configuration file
+#cp /data/.oh-my-tmux_d/.tmux.conf.local ~/.config/tmux/tmux.conf.local
+
+wget -4 -O ~/.config/tmux/tmux.conf.local  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmux_d/tmux.conf.local
+
+echo 'tmux source-file ~/.config/tmux/tmux.conf' >> ~/.bashrc
+
+# Reload the tmux configuration
+tmux source-file ~/.config/tmux/tmux.conf
+
+# Start a new tmux session
+#tmux new-session
+
