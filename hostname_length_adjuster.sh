050d42a6cf7bf70604f205c997a93e04b8b3d2fa
HelloWorldWinning <feedzhulei@aol.com>
2024-04-26 00:07:17 +0800

    Fri Apr 26 12:07:17 AM CST 2024

--- hostname_length_adjuster.sh
+++ hostname_length_adjuster.sh
@@ -13,7 +13,7 @@ spaces=$(printf '%*s' $hostname_length)
 # Update the template in the JSON file
 # This requires jq - a command-line JSON processor
 # \u2B95  \u2B95
-jq '.blocks[1].segments[0].template = "'"$spaces \u27A4"'"' $config_path > temp.json && mv temp.json $config_path
+jq '.blocks[1].segments[0].template = "'"$spaces   \u27A4"'"' $config_path > temp.json && mv temp.json $config_path
 
 # Now you can initialize Oh-My-Posh
 eval "$(oh-my-posh init bash --config $config_path)"
