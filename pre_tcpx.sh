4d6be3a674782c784ea946efd15fda08e8291867
HelloWorldWinning <feedzhulei@aol.com>
2024-04-26 15:35:38 +0800

    Fri Apr 26 03:35:38 PM CST 2024

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -147,7 +147,7 @@ function check_setup_time() {
 
 html_content=\$(curl -m 10 -s 'http://www.weather.com.cn/weather/101040100.shtml')
 weather=\$(echo "\$html_content" |   grep -oP '(?<=class="wea">).*?(?=</p>)' |head -n2 | tr '\n' ';' | sed 's/;\$//'  )
-weather=$(echo $weather | tr ';' '_')
+weather=\$(echo \$weather | tr ';' '_')
 temperature=\$(echo "\$html_content" | grep -oP '(?<=<i>).*?(?=℃</i>)' |head -n 1 ) 
 we_temp="\${temperature}°C \${weather}"
 ##weather_temperature="\${temperature}°C \${weather}"
