3a3e0831aa1efb0e7f7ad70817eddcde68f704f0
HelloWorldWinning <feedzhulei@aol.com>
2024-05-11 17:50:07 +0800

    Sat May 11 05:50:07 PM CST 2024

--- goodv3.sh
+++ goodv3.sh
@@ -655,6 +655,7 @@ ${Red_font_prefix}ax${Font_color_suffix} markdown.sh 177 render
 ${Red_font_prefix}b${Font_color_suffix} config_fzf.sh   install fzf
 ${Red_font_prefix}c${Font_color_suffix} tmux_d/tmux_install.sh
 ${Red_font_prefix}d${Font_color_suffix} share_data_to_accessor.sh
+${Red_font_prefix}e${Font_color_suffix} install_ftp_server.sh
 ${Red_font_prefix}q${Font_color_suffix} exit
 sed -i 's/eth0/enp0s3/g'  /etc/sysctl.conf 
 nohup command > /dev/null 2>&1 &
@@ -899,6 +900,7 @@ netstat -tulnp | grep -E '166|155'
 		b) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/fzf_d/config_fzf.sh  )  ;;
 		c) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmux_d/tmux_install.sh )  ;;
 		d) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/share_data_to_accessor.sh  )  ;;
+		e) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_ftp_server.sh   )  ;;
 		666) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/temp_bash_d/2024-03-20_14-32-34.sh  )  ;;
 
 #	00)eval "exit";;
---
+++ install_ftp_server.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Prompt for username, default to 'ftpuser' if nothing is input
+read -p "Enter the username for the FTP user (default: ftpuser): " ftpuser
+ftpuser=${ftpuser:-ftpuser}
+
+# Create the FTP user
+sudo adduser --home /home/$ftpuser --shell /bin/false $ftpuser
+
+# Create the FTP directory
+sudo mkdir -p /home/$ftpuser/ftp
+
+# Set ownership and permissions for the FTP directory
+#sudo chown nobody:nogroup /home/$ftpuser/ftp
+sudo chown nobody:nogroup /home/$ftpuser
+sudo chmod a-w /home/$ftpuser/ftp
+
+# Remove any existing vsftpd package
+sudo apt remove vsftpd -y
+sudo apt purge vsftpd -y
+
+# Install vsftpd
+sudo apt install vsftpd -y
+
+# Backup the existing vsftpd configuration file
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+
+# Create a new vsftpd configuration file
+sudo cat <<EOF >> /etc/vsftpd.conf
+local_root=/home/$ftpuser/ftp
+chroot_local_user=YES
+allow_writeable_chroot=YES
+pam_service_name=ftp
+EOF
+
+
+
+# Restart vsftpd service
+sudo systemctl restart vsftpd
+sleep 1
+
+sudo systemctl status vsftpd
+
+echo "FTP server installation completed."
