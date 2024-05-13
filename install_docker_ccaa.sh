0427bf6dca90ef3fbe6ae425b8b2791859a6910b
HelloWorldWinning <feedzhulei@aol.com>
2024-05-14 02:12:30 +0800

    Tue May 14 02:12:30 AM CST 2024

--- install_docker_ccaa.sh
+++ install_docker_ccaa.sh
@@ -1,5 +1,5 @@
 docker run --name="ccaa" -d  --restart=always    -p 6080:6080 -p 6081:6081 -p 6800:6800 -p 51413:51413 \
     -v /data/ccaaDown:/data/ccaaDown\
-    -e PASS="1" \
+    -e PASS="a" \
     helloz/ccaa \
     sh -c "dccaa pass && dccaa start"
