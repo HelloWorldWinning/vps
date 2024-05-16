fb30aa0bcfbabc0b79361daee5c984415a18314e
HelloWorldWinning <feedzhulei@aol.com>
2024-05-17 00:56:50 +0800

    Fri May 17 12:56:50 AM CST 2024

---
+++ weather_temperature.sh
@@ -0,0 +1,6 @@
+html_content=$(curl -m 10 -s 'http://www.weather.com.cn/weather/101040100.shtml')
+weather=$(echo "$html_content" |   grep -oP '(?<=class="wea">).*?(?=</p>)' |head -n2 | tr '\n' ';' | sed 's/;$//'  )
+weather=$(echo $weather | tr ';' '_')
+temperature=$(echo "$html_content" | grep -oP '(?<=<i>).*?(?=℃</i>)' |head -n 1 ) 
+export we_temp="${temperature}°C ${weather}"
+export weather_temperature="${temperature}${weather}"
