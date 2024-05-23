9c49fbcbd953adb12bbd3571f668eb2999e4011c
HelloWorldWinning <feedzhulei@aol.com>
2024-05-23 18:11:48 +0800

    Thu May 23 06:11:48 PM CST 2024

---
+++ upload_folder_d/run_docker_compose.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the desired folder name
+folder_name="upload_service_docker_compose_folder"
+
+# Check if the folder already exists
+if [ -d "$folder_name" ]; then
+  echo "Error: Folder '$folder_name' already exists."
+  exit 1
+fi
+
+# Create the new folder
+mkdir "$folder_name"
+
+# Change to the new folder
+cd "$folder_name"
+
+# Download the docker-compose.yml file
+curl -4O https://raw.githubusercontent.com/HelloWorldWinning/vps/main/upload_folder_d/docker-compose.yml
+
+# Run docker-compose
+docker-compose up -d
