7bd720719810f706ea370a6eb87472e7d052b7c6
HelloWorldWinning <feedzhulei@aol.com>
2024-05-23 18:16:14 +0800

    Thu May 23 06:16:14 PM CST 2024

--- goodv3.sh
+++ goodv3.sh
@@ -644,7 +644,7 @@ ${Red_font_prefix}102${Font_color_suffix} 中文编码问题 txt
 ${Red_font_prefix}103${Font_color_suffix} bash  <(curl -sLk  yabs.sh  ) -i
 ${Red_font_prefix}104${Font_color_suffix} TA-Lib install
 ${Red_font_prefix}105${Font_color_suffix} git new branch.sh 
-${Red_font_prefix}106${Font_color_suffix} upload_folder.sh
+${Red_font_prefix}106${Font_color_suffix} docker:upload_folder.sh
 ${Red_font_prefix}107${Font_color_suffix} curl -sL yabs.sh | bash -s -- -i
 ${Red_font_prefix}108${Font_color_suffix} ddp_master.sh pytorch ddp distribution  computing
 ${Red_font_prefix}109${Font_color_suffix} ray_install3.sh
@@ -897,7 +897,8 @@ netstat -tulnp | grep -E '166|177'
 		103) bash  <(curl -sLk  yabs.sh  ) -i ;;
 		104) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ta-lib.sh  )  ;;
 		105) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/branch.sh  )  ;;
-		106) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/upload_folder.sh )  ;;
+		1066) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/upload_folder.sh )  ;;
+		106)  bash  <(curl -4Lsk  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/upload_folder_d/run_docker_compose.sh)    ;;
 		107) curl -sL yabs.sh | bash -s -- -i ;;
 
 		108) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ddp_master.sh )  ;;
