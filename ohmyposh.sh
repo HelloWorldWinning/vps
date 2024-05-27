e9fe9619d75047b0ffa801e4a12d44e18b221c10
HelloWorldWinning <feedzhulei@aol.com>
2024-05-27 13:36:10 +0800

    Mon May 27 01:36:10 PM CST 2024

--- ohmyposh.sh
+++ ohmyposh.sh
@@ -4,7 +4,8 @@ curl -sSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/weather_t
 
 
 # Define the cron job command
-cron_job_wea="0 */1 * * * curl -sSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/weather_temperature.sh | bash"
+##cron_job_wea="0 */1 * * * curl -sSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/weather_temperature.sh | bash"
+cron_job_wea="*/30 * * * * curl -sSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/weather_temperature.sh | bash"
 
 # Check if weather_temperature.sh is already in the crontab
 if ! crontab -l | grep -q "weather_temperature.sh"; then
