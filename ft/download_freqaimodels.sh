f4f58954e919df47fdcb24d59c1cb197262b2b52
root <feedzhulei@aol.com>
2024-06-19 14:57:03 +0800

    Wed Jun 19 02:57:03 PM CST 2024

--- ft/download_freqaimodels.sh
+++ ft/download_freqaimodels.sh
@@ -15,5 +15,24 @@ curl -s $API_URL | jq -r '.[] | .download_url' | while read file_url; do
     curl -L $file_url -o "$LOCAL_DIR/$(basename $file_url)"
 done
 
-echo "Download completed."
+echo "freqaimodels_d Download completed."
 
+
+#################
+
+# GitHub API URL for the content of the directory
+API_URL="https://api.github.com/repos/HelloWorldWinning/vps/contents/ft/info_d?ref=main"
+
+# Local directory where files will be saved
+LOCAL_DIR="info_d"
+
+# Create the local directory if it does not exist
+mkdir -p "$LOCAL_DIR"
+
+# Use GitHub API to list files in the directory and then download each one
+curl -s $API_URL | jq -r '.[] | .download_url' | while read file_url; do
+    echo "Downloading $file_url"
+    curl -L $file_url -o "$LOCAL_DIR/$(basename $file_url)"
+done
+
+echo "info_d Download completed."
