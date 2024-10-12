3ee308273ec126656b99eb7397fd082d5733749f
root <feedzhulei@aol.com>
2024-10-12 15:11:59 +0800

    Sat Oct 12 03:11:59 PM CST 2024

---
+++ typecho-theme-simple_bak/footer.php
@@ -0,0 +1,16 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+
+
+<footer>
+<div class="footer container">
+    &copy; <?php echo date('Y'); ?> <?php _e('All Rights Reserved, Powered By <a href="http://typecho.org" target="_blank">Typecho</a>, Theme <a href="http://lhcy.org" target="_blank">Simple</a>.'); ?>
+</div>
+</footer>
+
+        </div>
+</div>
+
+<?php $this->footer(); ?>
+</body>
+</html>
