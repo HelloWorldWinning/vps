43ece02c5e0ad4d6bf7106e59c6d1ae75458f24b
root <stone100100@gmail.com>
2024-10-22 21:51:16 +0800

    Tue Oct 22 09:51:16 PM CST 2024

--- update_prompt_date_DD_Day.sh
+++ update_prompt_date_DD_Day.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-#  # 
-#  # hexagrams=(䷀ ䷁ ䷂ ䷃ ䷄ ䷅ ䷆ ䷇ ䷈ ䷉ ䷊ ䷋ ䷌ ䷍ ䷎ ䷏ ䷐ ䷑ ䷒ ䷓ ䷔ ䷕ ䷖ ䷗ ䷘ ䷙ ䷚ ䷛ ䷜ ䷝ ䷞ ䷟ ䷠ ䷡ ䷢ ䷣ ䷤ ䷥ ䷦ ䷧ ䷨ ䷩ ䷪ ䷫ ䷬ ䷭ ䷮ ䷯ ䷰ ䷱ ䷲ ䷳ ䷴ ䷵ ䷶ ䷷ ䷸ ䷹ ䷺ ䷻ ䷼ ䷽ ䷾ ䷿)
-#  # 
-#  # random_index=$((RANDOM % 64))
-#  # second_part="${hexagrams[random_index]} "
-
-
-
-#echo "second_part=\"$second_part\""
-# First part: Current date in DD-Day format
-#first_part=$(date +"%d-%a")
 first_part=$(date +"%I:%M %d-%a")
-
-# Array of symbols
 symbols=(" ☯" " ☰" " ☱" " ☲" " ☳" " ☴" " ☵" " ☶" " ☷")
-# Randomly pick one
-second_part="${symbols[$RANDOM % ${#symbols[@]}]}"
+config_file="/root/themes/gmay3.omp.json"
+symbol_state_file="/tmp/current_symbol.txt"
 
-# Output the randomly selected symbol with the format
-#echo "$second_part"
+# Function to check if 30 minutes have passed since last symbol change
+need_symbol_update() {
+    if [ ! -f "$symbol_state_file" ]; then
+        return 0  # File doesn't exist, needs update
+    fi
+    
+    last_update=$(stat -c %Y "$symbol_state_file")
+    current_time=$(date +%s)
+    time_diff=$((current_time - last_update))
+    
+    # Return 0 (true) if 30 minutes (1800 seconds) have passed
+    [ $time_diff -ge 1800 ]
+}
 
+# Get or update the symbol
+if need_symbol_update; then
+    second_part="${symbols[$RANDOM % ${#symbols[@]}]}"
+    echo "$second_part" > "$symbol_state_file"
+else
+    second_part=$(cat "$symbol_state_file")
+fi
 
-# Second part: Mountain emoji
-#second_part="䷀ " 
-#second_part="⛰️ " 
-#second_part="🔥"
-
-# Combine the parts
-#combined_template="    $first_part $second_part"
 combined_template="$first_part$second_part"
 
-# Path to your Oh My Posh configuration file
-config_file="/root/themes/gmay3.omp.json"
-
-# Use jq to update the template in the JSON structure
+# Update the config file
 jq --arg new_template "$combined_template" '
-  .blocks[1].segments[0].template = $new_template
+    .blocks[1].segments[0].template = $new_template
 ' "$config_file" > "${config_file}.tmp" && mv "${config_file}.tmp" "$config_file"
 
 echo "Updated template to '$combined_template' in $config_file"
