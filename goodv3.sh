3c393cbd34c41a020b9bcf2465f10587173e1520
root <stone100100@gmail.com>
2024-10-24 17:49:32 +0800

    Thu Oct 24 05:49:32 PM CST 2024

--- goodv3.sh
+++ goodv3.sh
@@ -803,8 +803,8 @@ yes | bash  <(curl --ipv4 -Ls  https://raw.githubusercontent.com/HelloWorldWinni
                 3)eval "$xray_mianliu" ;;
                 411)eval "$pre_InstallNET_modified_chu" ;;
                 4)eval "$pre_InstallNET_modified_chu_debian12" ;;
-                44)  bash <(wget -4  --no-check-certificate -qO- 'https://raw.githubusercontent.com/HelloWorldWinning/vps/main/CHU_reinstall_debian_current.sh ')  ;;
-		444) bash <(wget --inet4-only --no-check-certificate -qO- 'https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_debian_current_netboot.sh' )    ;; 
+                444)  bash <(wget -4  --no-check-certificate -qO- 'https://raw.githubusercontent.com/HelloWorldWinning/vps/main/CHU_reinstall_debian_current.sh ')  ;;
+		4444) bash <(wget --inet4-only --no-check-certificate -qO- 'https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_debian_current_netboot.sh' )    ;; 
                 65)eval "$ss_rust2" ;;
           #     66)eval "$ping_local_fast" ;;
 #        66) bash  <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/ping_ask.sh ) ;;
