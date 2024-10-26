eb13bbe96c2a68f0bb00ae5cf619e36d9c87aafe
root <stone100100@gmail.com>
2024-10-26 18:56:43 +0800

    Sat Oct 26 06:56:43 PM CST 2024

--- calibre_linux_setup_ebook_online_reader.sh
+++ calibre_linux_setup_ebook_online_reader.sh
@@ -77,6 +77,8 @@ Type=simple
 User=root
 Group=root
 Environment="PATH=/opt/calibre/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+ExecStart=/usr/bin/calibre-server /data/calibre-library --port=188 --enable-auth --disable-use-bonjour --log  /var/log/calibre-server.log --access-log  /var/log/calibre-access.log --compress-min-size=31457280 --disable-use-sendfile
 ExecStart=/usr/bin/calibre-server /data/calibre-library --port=188 --enable-auth --disable-use-bonjour
 StandardOutput=append:/var/log/calibre-server.log
 StandardError=append:/var/log/calibre-server.error.log
