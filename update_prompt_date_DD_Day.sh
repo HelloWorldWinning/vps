21d74cb485b1ef3a1ba84940f4f2061ff0672ca3
root <stone100100@gmail.com>
2024-10-28 17:30:58 +0800

    Mon Oct 28 05:30:58 PM CST 2024

---
+++ update_prompt_date_DD_Day.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+something=30  # Time interval in minutes for updating second_part
+
+first_part=$(date +"%I:%M %d-%a")
+
+symbols=(" ☰" " ☱" " ☲" " ☳" " ☴" " ☵" " ☶" " ☷")
+config_file="/root/themes/gmay3.omp.json"
+symbol_state_file="/tmp/current_symbol.txt"
+
+# Function to check if 'something' minutes have passed since last symbol change
+need_symbol_update() {
+    if [ ! -f "$symbol_state_file" ]; then
+        return 0  # File doesn't exist, needs update
+    fi
+
+    last_update=$(stat -c %Y "$symbol_state_file")
+    current_time=$(date +%s)
+    time_diff=$((current_time - last_update))
+
+    interval_seconds=$((something * 60))
+
+    # Return 0 (true) if 'something' minutes have passed
+    [ $time_diff -ge $interval_seconds ]
+}
+
+# Get or update the symbol
+if need_symbol_update; then
+    second_part="${symbols[$RANDOM % ${#symbols[@]}]}"
+    echo "$second_part" > "$symbol_state_file"
+else
+    second_part=$(cat "$symbol_state_file")
+fi
+
+combined_template="    $first_part$second_part"
+
+# Update the config file
+jq --arg new_template "$combined_template" '
+    .blocks[1].segments[0].template = $new_template
+' "$config_file" > "${config_file}.tmp" && mv "${config_file}.tmp" "$config_file"
+
+echo "Updated template to '$combined_template' in $config_file"
+
