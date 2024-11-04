ba3a39be0b7e192e09e5d3973d6be1ef893ae15c
root <stone100100@gmail.com>
2024-11-04 22:41:29 +0800

    Mon Nov  4 10:41:29 PM CST 2024

--- upload_folder_d/run_docker_compose.sh
+++ upload_folder_d/run_docker_compose.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Set the desired folder name
-#folder_name="upload_service_docker_compose_folder"
-folder_name="/data/upload_service_docker_compose_folder"
+#
+##folder_name="upload_service_docker_compose_folder"
+##folder_name="/data/upload_service_docker_compose_folder"
+folder_name="/root/upload_service_docker_compose_folder"
 
 # Check if the folder already exists
-if [ -d "$folder_name" ]; then
-  echo "Error: Folder '$folder_name' already exists."
-  exit 1
-fi
+#if [ -d "$folder_name" ]; then
+#  echo "Error: Folder '$folder_name' already exists."
+#  exit 1
+#fi
 
 # Create the new folder
-mkdir "$folder_name"
+mkdir -p  "$folder_name"
 
 # Change to the new folder
 cd "$folder_name"
@@ -20,6 +22,8 @@ cd "$folder_name"
 curl -4O https://raw.githubusercontent.com/HelloWorldWinning/vps/main/upload_folder_d/docker-compose.yml
 
 # Run docker-compose
+docker-compose down
+docker-compose pull
 docker-compose up -d
 echo "sleep 1s ..."
 sleep 1
