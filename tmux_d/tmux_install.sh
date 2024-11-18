d428309b76ee7f7303d71ad165d2efa8d2157367
root <stone100100@gmail.com>
2024-11-18 13:43:07 +0800

    Mon Nov 18 01:43:07 PM CST 2024

--- tmux_d/tmux_install.sh
+++ tmux_d/tmux_install.sh
@@ -22,6 +22,13 @@ wget -4 -O ~/.config/tmux/tmux.conf.local  https://raw.githubusercontent.com/Hel
 
 wget -4 -O ~/.config/tmux/python_version.sh   https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmux_d/python_version.sh
 
+wget -4 -O ~/.config/tmux/get_country_flag.sh     https://raw.githubusercontent.com/HelloWorldWinning/vps/main/get_country_flag.sh 
+
+#curl -4SsL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/get_country_flag.sh  >  ~/themes/get_country_flag.sh
+#chmod 777  ~/themes/get_country_flag.sh     
+
+
+
 #######echo 'tmux source-file ~/.config/tmux/tmux.conf' >> ~/.bashrc
 
 if ! grep -q 'tmux source-file ~/.config/tmux/tmux.conf' ~/.bashrc; then
