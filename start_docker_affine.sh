6bbcd40703e9c65ac0bfce0ba6b63232cfc0612c
root <stone100100@gmail.com>
2024-11-25 23:07:52 +0800

    Mon Nov 25 11:07:52 PM CST 2024

---
+++ start_docker_affine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Path_AFFiNE='/root/AFFiNE_D'
+mkdir -p $Path_AFFiNE/self-host/{config,storage,redis,postgres}
+
+###curl -fSsL4 https://raw.githubusercontent.com/HelloWorldWinning/vps/main/compose_yml_AFFiNE.yml -o $Path_AFFiNE/compose.yml
+curl  -o  $Path_AFFiNE/compose.yml       -fSsL4   https://raw.githubusercontent.com/HelloWorldWinning/vps/main/compose_yml_AFFiNE.yml
+
+cd $Path_AFFiNE
+docker compose up -d
+
+sleep 5
+
+echo "Container Status:"
+docker ps --format "table {{.Names}}\t{{.Status}}\t{{.State}}" | grep "affine"
