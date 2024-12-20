a00f90b946cfdf8e02c64e794ed4910e10167058
root <stone100100@gmail.com>
2024-12-20 15:44:23 +0800

    Fri Dec 20 03:44:23 PM CST 2024

--- one_file_transfer4.sh
+++ one_file_transfer4.sh
@@ -17,10 +17,16 @@ print_status() {
 }
 
 # Function to print success messages
+#print_success() {
+#   #echo -e "${GREEN_BG}${BLACK_TEXT}${BOLD} SUCCESS ${RESET} $1"
+#    echo -e "${RED_BG}${BOLD} SUCCESS ${RESET} $1"
+#}
+
+# Function to print success messages
 print_success() {
-   #echo -e "${GREEN_BG}${BLACK_TEXT}${BOLD} SUCCESS ${RESET} $1"
-    echo -e "${RED_BG}${BOLD} SUCCESS ${RESET} $1"
+    echo -e "${RED_FONT}${BOLD}SUCCESS${RESET} $1"
 }
+
 
 # Function to calculate and display MD5
 calculate_md5() {
