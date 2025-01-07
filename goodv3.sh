267bb65e1c67f9a6f1a76d4ba2b316f30cb99c6f
root <stone100100@gmail.com>
2025-01-07 19:13:42 +0800

    Tue Jan  7 07:13:42 PM CST 2025

--- goodv3.sh
+++ goodv3.sh
@@ -1071,6 +1071,7 @@ netstat -tulnp | grep -E '166|177'
 		d443) bash  <(curl -4LSs  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/outline_d/outline_setup_openai.sh ) ;; 
 		udp54000) bash  <(curl -4LSs  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/udp_redirect54000_54999.sh ) ;; 
 		run3g) bash  <(curl -4LSs  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmpfs_manager_run.sh ) ;; 
+		dd) yes |docker system prune -a --volumes  ;;
 
 #	00)eval "exit";;
 		q)eval "exit";;
