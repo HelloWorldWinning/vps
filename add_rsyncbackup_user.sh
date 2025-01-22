f12716551498c833b029c953ad1777a776d1f860
root <stone100100@gmail.com>
2025-01-23 00:34:48 +0800

    Thu Jan 23 12:34:48 AM CST 2025

--- add_rsyncbackup_user.sh
+++ add_rsyncbackup_user.sh
@@ -86,8 +86,9 @@ log "- The rsyncbackup user can run necessary commands without password."
 
 # Get and display hostname information
 HOSTNAME=$(hostname)
+log "-----------------------------------------------------"
 log "Current server hostname: $HOSTNAME"
-log "-----------------------------"
+log "-----------------------------------------------------"
 log "If this is your backup SOURCE server (VPS1), enter 'y' to continue with backup setup."
 log "If this is your backup DESTINATION server (VPS2), enter 'n' to finish setup."
 
