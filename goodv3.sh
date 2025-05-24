42ae73d8ff4234ed33b810e5dce046974ab77df1
root <stone100100@gmail.com>
2025-05-24 16:05:54 +0800

    Sat May 24 04:05:54 PM CST 2025

--- goodv3.sh
+++ goodv3.sh
@@ -383,12 +383,12 @@ sysctl -p
 #all_hit=$(echo "${today_all}"|cut -d" " -f2)
 
 
-# METHOD 2 - Pattern matching (8ms, but more reliable)
-response=$(curl -s --max-time 10 "https://hitscounter.dev/api/hit?url=https%3A%2F%2Fraw.githubusercontent.com%2FHelloWorldWinning%2Fvps%2Fmain%2Fgoodv3.sh&label=try&icon=alarm&color=%23198754")
-hit_data=$(echo "$response" | grep -o '[0-9]* / [0-9]*' | head -1)
-today_hit=$(echo "$hit_data" | cut -d' ' -f1)
-all_hit=$(echo "$hit_data" | cut -d' ' -f3)
-
+## METHOD 2 - Pattern matching (8ms, but more reliable)
+#response=$(curl -s --max-time 10 "https://hitscounter.dev/api/hit?url=https%3A%2F%2Fraw.githubusercontent.com%2FHelloWorldWinning%2Fvps%2Fmain%2Fgoodv3.sh&label=try&icon=alarm&color=%23198754")
+#hit_data=$(echo "$response" | grep -o '[0-9]* / [0-9]*' | head -1)
+#today_hit=$(echo "$hit_data" | cut -d' ' -f1)
+#all_hit=$(echo "$hit_data" | cut -d' ' -f3)
+#
 
 
 
