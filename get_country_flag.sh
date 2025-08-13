e70787963b465e835ffb9dabb624bd1950c529df
root <stone100100@gmail.com>
2025-08-13 18:27:35 +0800

    Wed Aug 13 06:27:35 PM CST 2025

--- get_country_flag.sh
+++ get_country_flag.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#echo -e "\\U$(printf '%x' $(($(printf '%d' "'${country_code:0:1}") + 127397)))\\U$(printf '%x' $(($(printf '%d' "'${country_code:1:1}") + 127397)))"
-echo  $country_code
+
+if [[ -f /etc/debian_version ]] && [[ $(cat /etc/debian_version | cut -d. -f1) == "13" ]]; then
+	echo $country_code
+else
+	echo -e "\\U$(printf '%x' $(($(printf '%d' "'${country_code:0:1}") + 127397)))\\U$(printf '%x' $(($(printf '%d' "'${country_code:1:1}") + 127397)))"
+fi
