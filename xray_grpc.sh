36a47aa7224004fcd9d82969a7417433d96b5de6
root <stone100100@gmail.com>
2025-09-20 13:39:54 +0800

    Sat Sep 20 01:39:54 PM CST 2025

--- xray_grpc.sh
+++ xray_grpc.sh
@@ -1,12 +1,13 @@
-
 ports_to_one(){
 apt-get install iptables-persistent -y
 
 net_card=$(ip addr |grep BROADCAST|head -1|awk '{print $2; exit}'|cut -d ":" -f 1)
 
-iptables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:59999 -j DNAT --to-destination :${Port}
-ip6tables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :${Port}
+#iptables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:59999 -j DNAT --to-destination :${Port}
+#ip6tables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 55000:60000 -j DNAT --to-destination :${Port}
 
+iptables -t  nat -A PREROUTING -i ${net_card} -p tcp --dport 59000:60000 -j DNAT --to-destination :${Port}
+ip6tables -t nat -A PREROUTING -i ${net_card} -p tcp --dport 59000:60000 -j DNAT --to-destination :${Port}
 
 iptables-save -f /etc/iptables/rules.v4
 ip6tables-save -f /etc/iptables/rules.v6
