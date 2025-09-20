48c8be2ad1bfcb7d4131165da2d572e1bc650da4
root <stone100100@gmail.com>
2025-09-21 00:10:43 +0800

    Sun Sep 21 12:10:43 AM CST 2025

--- goodv3.sh
+++ goodv3.sh
@@ -1142,7 +1142,7 @@ netstat -tulnp | grep -E '166|177'
 		gdu) bash <(curl -fSsL4  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/dust_gdu_install.sh  )  ;;
 		p) bash <(curl -fSsL4  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/pushing_files.sh )  ;;
 		netbird_de) bash <(curl -fSsL4  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/deregister_netbird.sh )  ;;
-		re_info) bash <(curl -fSsL4  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/redirect_info.sh  )  ;;
+		reinfo) bash <(curl -fSsL4  https://raw.githubusercontent.com/HelloWorldWinning/vps/main/redirect_info.sh  )  ;;
 
 
 
