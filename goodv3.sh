0278d8ac0fc5cfeecf473bd5e0b754008d42005f
root <stone100100@gmail.com>
2025-12-06 15:45:59 +0800

    Sat Dec  6 03:45:59 PM CST 2025

--- goodv3.sh
+++ goodv3.sh
@@ -725,7 +725,7 @@ ${Red_font_prefix}h${Font_color_suffix} init_github_project.sh
 ${Red_font_prefix}run3g${Font_color_suffix} tmpfs_manager_run.sh
 ${Red_font_prefix}write${Font_color_suffix} install_dc install_affine install_notepad install_flatnote install_wf install_standard_note  install_standard_note_443
 ${Red_font_prefix}some${Font_color_suffix}  neo4j   journald_conf_set docu   outline_coll 
-${Red_font_prefix}some${Font_color_suffix}  xray_reality xray_tls_gp   xray_tls_cl  test_reality  up_v6_gcp_gm  up_v6_gcp_gp
+${Red_font_prefix}some${Font_color_suffix}  xray_reality xray_tls_gp   xray_tls_cl  test_reality  up_v6_gcp_gm  up_v6_gcp_gp  xray_xhttp  xray_grpc  xxr
 ${Red_font_prefix}q${Font_color_suffix} exit
 sed -i 's/eth0/enp0s3/g'  /etc/sysctl.conf 
 nohup command > /dev/null 2>&1 &
