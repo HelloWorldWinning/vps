96ed518ec5ae2b34522920b9d5c30c58b7568322
root <stone100100@gmail.com>
2026-03-11 16:53:37 +0800

    Wed Mar 11 04:53:37 PM CST 2026

--- goodv3.sh
+++ goodv3.sh
@@ -628,8 +628,10 @@ ${Red_font_prefix}49${Font_color_suffix} shadowrocket rust + many plugins(kcptun
 ${Red_font_prefix}8${Font_color_suffix} eval "netstat -lpntu"
 ${Red_font_prefix}tt${Font_color_suffix} "read -p "script to run ": ${x}"
 ${Red_font_prefix}56${Font_color_suffix} rdp_docker_33399.sh
+${Red_font_prefix}rdp${Font_color_suffix} rdp_docker_33399.sh
 ${Red_font_prefix}33389${Font_color_suffix} install_rdesktop_33389.sh  abc:abc
 ${Red_font_prefix}566${Font_color_suffix} (amd64)一键安装 远程桌面 echo xfce4-session>/home/<rdp_username>/.xsession ; sudo service xrdp stop /status
+${Red_font_prefix}rdpcli${Font_color_suffix} (amd64)一键安装 远程桌面 echo xfce4-session>/home/<rdp_username>/.xsession ; sudo service xrdp stop /status
 ${Red_font_prefix}57${Font_color_suffix} delete user  'getent passwd | awk -F: '{ print \$1}'|sort'
 ${Red_font_prefix}58${Font_color_suffix} bash <(curl -fsSL https://git.io/hysteria.sh)
 ${Red_font_prefix}581${Font_color_suffix} install_hysteria HyNetwork  
@@ -848,7 +850,11 @@ $(
 	500) eval ps_filter ;;
 	r) read -p 'script to run': x && ${x} ;;
 	566) eval "$rdp" ;;
+	rdpcli) eval "$rdp" ;;
 	56)
+		bash <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/rdp_docker_33399.sh)
+		;;
+	rdp)
 		bash <(curl --ipv4 -Ls https://raw.githubusercontent.com/HelloWorldWinning/vps/main/rdp_docker_33399.sh)
 		;;
 	33389)
