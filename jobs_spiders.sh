193625cfc9072556a014684fa1bbccd00a248110
root <stone100100@gmail.com>
2026-03-20 18:45:42 +0800

    Fri Mar 20 06:45:42 PM CST 2026

--- jobs_spiders.sh
+++ jobs_spiders.sh
@@ -2,30 +2,23 @@
 
 mkdir /root/job_2021
 apt install -y aptitude
-aptitude install  -y chromium-driver  msmtp mutt
-
+aptitude install -y chromium-driver msmtp mutt
 
 cat >>~/.bashrc <<EOF
 export PATH="/root/anaconda3/bin:$PATH"
 EOF
-source ~/.bashrc 
+source ~/.bashrc
 
-
-version=$(curl https://repo.anaconda.com/archive/ |grep Linux-x86_64.sh|cut -d">" -f2|cut -d'"' -f2  |head -1)
+version=$(curl https://repo.anaconda.com/archive/ | grep Linux-x86_64.sh | cut -d">" -f2 | cut -d'"' -f2 | head -1)
 URL=$(echo "https://repo.anaconda.com/archive/$version")
 #echo $URL
 
-
 #wget -O /root/Anaconda3-2022.05-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 #wget -O /root/Anaconda-Linux-x86_64.sh           https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-wget -O /root/Anaconda3-Linux-x86_64.sh     "${URL}"      
-bash    /root/Anaconda3-Linux-x86_64.sh
+wget -O /root/Anaconda3-Linux-x86_64.sh "${URL}"
+bash /root/Anaconda3-Linux-x86_64.sh
 
-pip install  html5lib selenium
-
-
-
- 
+pip install html5lib selenium
 
 cat >>/root/.msmtprc <<EOF
 tls on
@@ -39,45 +32,33 @@ host smtp.gmail.com
 domain gmail.com
 port 587
 logfile   ~/.msmtp.log
-user work100100@gmail.com
-from work100100@gmail.com
-password vmxjjnvkhkhfdqsp
-#password qiubojunKJL12345
-#password mmamhaodrvokcvhc
+user   @gmail.com
+from   @gmail.com
+password vmxjj
 #tls_trust_file /etc/ssl/certs/ca-certificates.crt
 # account gmail
 
 EOF
- 
-
 
 cat >>/root/.muttrc <<EOF
 set editor="vim"
 set sendmail="/usr/bin/msmtp"
 set use_from=yes
-set realname="work100100@gmail"
-set from=work100100@gmail.com
+set realname="@gmail"
+set from=@gmail.com
 set envelope_from=yes
 set crypt_use_gpgme=no
 
 EOF
-
-
-
-
 
 ################################################
 #aptitude install  -y chromium-driver  msmtp mutt
 #cat >>~/.bashrc <<EOF
 #export PATH="/root/anaconda3/bin:$PATH"
 #EOF
-#source ~/.bashrc 
+#source ~/.bashrc
 #pip install  html5lib selenium
 
+/root/anaconda3/bin/pip install python-telegram-bot --upgrade
 
-
-
-
-/root/anaconda3/bin/pip  install python-telegram-bot --upgrade
-
-rm /root/Anaconda3-Linux-x86_64.sh 
+rm /root/Anaconda3-Linux-x86_64.sh
