4ce68bf4f67be540f36c59c1c3d8a995c97515aa
root <stone100100@gmail.com>
2026-04-22 18:13:27 +0800

    Wed Apr 22 06:13:27 PM CST 2026

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -592,8 +592,8 @@ bash <(curl -s https://raw.githubusercontent.com/kamiyaa/joshuto/master/utils/in
 
 # 1. Download to /usr/local/bin (in $PATH by default on Debian)
 sudo wget -O /usr/local/bin/tagcat https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tagcat
-
-# 2. Make it executable
 sudo chmod +x /usr/local/bin/tagcat
 
+sudo wget -O /usr/local/bin/tokencount https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tokencount
+sudo chmod +x /usr/local/bin/tokencount
 sudo reboot
