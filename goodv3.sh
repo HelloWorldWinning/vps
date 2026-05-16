cb4f6446793e4a720a2504e425aea646ed0b01ae
root <stone100100@gmail.com>
2026-05-16 20:29:33 +0800

    Sat May 16 08:29:33 PM CST 2026

--- goodv3.sh
+++ goodv3.sh
@@ -1163,7 +1163,8 @@ https://zhile.one/archives/1404.html
 	copier) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/copier.sh) ;;
 	t) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tokentool) ;;
 		#mm) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/markmap.sh) ;;
-	mm) python <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/markmap.py) ;;
+		#mm) python <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/markmap.py) ;;
+	mm) curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/markmap.py -o /usr/local/bin/mm && chmod +x /usr/local/bin/mm ;;
 	wos) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/wos_bib) ;;
 	tag) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tagcat) ;;
 	webdav) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/WebDAV.sh) ;;
