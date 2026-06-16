6d4947a26c6e267931f488347830a010ed3817d7
root <stone100100@gmail.com>
2026-06-16 12:54:22 +0800

    Tue Jun 16 12:54:21 PM CST 2026

--- goodv3.sh
+++ goodv3.sh
@@ -1084,6 +1084,7 @@ https://zhile.one/archives/1404.html
 	a) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/markdown_render_dockerfile/markdown_render_docker.sh) ;;
 
 	b) bash <(curl -fSsL4 https://raw.githubusercontent.com/HelloWorldWinning/vps/main/fzf_d/config_fzf.sh) ;;
+	tmux) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmux_d/tmux_install.sh) ;;
 	cccc) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/tmux_d/tmux_install.sh) ;;
 	d) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/share_data_to_accessor.sh) ;;
 	e) bash <(curl -sL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_ftp_server.sh) ;;
