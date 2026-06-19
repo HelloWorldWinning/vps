715f82bf5da47908fb57b96078ecd7c940af54dd
root <stone100100@gmail.com>
2026-06-20 00:37:51 +0800

    Sat Jun 20 12:37:51 AM CST 2026

--- goodv3.sh
+++ goodv3.sh
@@ -1181,11 +1181,13 @@ https://zhile.one/archives/1404.html
 	acme_renew) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_acme-renew-all.sh) ;;
 	acme_install) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_acme.sh) ;;
 	install_acme) bash <(curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/install_acme.sh) ;;
-		#acme) bash /usr/local/bin/acme-manager ;;
-	acme) acme-manager ;;
-	ssl) acme-manager ;;
+	acme) bash /usr/local/bin/acme-manager ;;
+	ssl) bash /usr/local/bin/acme-manager ;;
 
+		########acme) acme-manager ;;
+		########ssl) acme-manager ;;
 		#nq) bash <(curl -sL https://run.NodeQuality.com) ;;
+
 	nq) bash <(curl -sL https://run.NodeQuality.com) 2>&1 | tee /root/nodequality_$(date +%Y%m%d_%H%M%S).log ;;
 
 	wos_bib) curl -4LSs https://raw.githubusercontent.com/HelloWorldWinning/vps/main/wos_bib -o /usr/local/bin/wos_bib && chmod +x /usr/local/bin/wos_bib ;;
