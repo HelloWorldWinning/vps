198f3a3f9c6cbac4cc69c131075734543c966d4d
root <stone100100@gmail.com>
2026-09-13 12:23:06 +0800

    Sun Sep 13 12:23:06 PM AWST 2026

--- pre_tcpx.sh
+++ pre_tcpx.sh
@@ -785,6 +785,8 @@ apt install vivid -y
 
 # curl -fsSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/kk.py -o /usr/local/bin/kk && chmod 0755 /usr/local/bin/kk && chmod +x /usr/local/bin/kk && ln -sf /usr/local/bin/kk /usr/local/bin/kkk
 
+curl -fsSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/kk -o /usr/local/bin/kk && chmod 0755 /usr/local/bin/kk && ln -sf /usr/local/bin/kk /usr/local/bin/kn && echo "kk installed success"
+
 curl -fsSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/i_color.py -o /usr/local/bin/i && chmod 0755 /usr/local/bin/i && ln -sf /usr/local/bin/i /usr/local/bin/ii
 
 curl -fsSL https://raw.githubusercontent.com/HelloWorldWinning/vps/main/m_color.py -o /usr/local/bin/k &&
